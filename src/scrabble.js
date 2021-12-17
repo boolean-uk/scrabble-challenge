f0ba4d45f0e69f2b2f0146e32420f2ac7cba0b2a
marianphillips <79763160+marianphillips@users.noreply.github.com>
2021-12-17 15:18:23 +0000

    first solution completed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,43 @@
 class Scrabble {
-  // first submision
+  constructor (word) {
+    this.word = word
+  }
+
+  score () {
+    let count = 0
+
+    if (this.word == null) {
+      return 0
+    }
+
+    for (let i = 0; i < this.word.length; i++) {
+      if (this.word[i].match(/[^A-Z]/i)) {
+        return 0
+      }
+      if (this.word[i].match(/[AEIOULNRST]/i)) {
+        count++
+      }
+      if (this.word[i].match(/[DG]/i)) {
+        count += 2
+      }
+      if (this.word[i].match(/[BCMP]/i)) {
+        count += 3
+      }
+      if (this.word[i].match(/[FHVWY]/i)) {
+        count += 4
+      }
+      if (this.word[i].match(/[K]/i)) {
+        count += 5
+      }
+      if (this.word[i].match(/[JX]/i)) {
+        count += 8
+      }
+      if (this.word[i].match(/[QZ]/i)) {
+        count += 10
+      }
+    }
+    return count
+  }
 }
 
 module.exports = Scrabble

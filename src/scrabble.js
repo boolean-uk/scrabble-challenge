b59cf3c4d75821ab4d388f6f0c740faa66b08744
PointWolfAnj <anojan31@gmail.com>
2021-12-17 15:19:27 +0000

    push

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,11 @@
+/* words = points, different letters have different points. letters together add up point in total   */
+
+
 class Scrabble {
   // Write your implementation here
-
+ 
 }
+
+
 
 module.exports = Scrabble

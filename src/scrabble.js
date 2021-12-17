ec6d67e5eb4c9f57f8ab9f2313c1563cbff8645b
Ben Corbin <ben@corbin.digital>
2021-12-17 17:14:50 +0000

    All tests passed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,12 +1,12 @@
-// const letterScores = {
-//   onepoint: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-//   twopoints: ['D', 'G'],
-//   threepoints: ['B', 'C', 'M', 'P'],
-//   fourpoints: ['F', 'H', 'V', 'W', 'Y'],
-//   fivepoints: ['K'],
-//   sixpoints: ['J', 'X'],
-//   sevenpoints: ['Q', 'Z']
-// } 
+const letterScores = {
+  onePoint: ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'l', 'L', 'n', 'N', 'r', 'R', 's', 'S', 't','T'],
+  twoPoints: ['d', 'D', 'g', 'G'],
+  threePoints: ['b', 'B', 'c', 'C', 'm', 'M', 'p', 'P'],
+  fourPoints: ['f', 'F', 'h', 'H', 'v', 'V', 'w', 'W', 'y', 'Y'],
+  fivePoints: ['k', 'K'],
+  eightPoints: ['j', 'J', 'x', 'X'],
+  tenPoints: ['q', 'Q', 'z', 'Z']
+}
 
 class Scrabble {
   constructor(word){
@@ -14,11 +14,37 @@ class Scrabble {
   }
 score() {
   //work out how to score here, probably with if statements
-  if (this.word.length === 0) {
-    return 0
+  let count = 0
+  if (this.word === null || this.word.length === 0 || this.word === " \t\n") {
+    return count
   }
-   
+  for (let i = 0; i < this.word.length; i++) {
+    if (letterScores.onePoint.includes(this.word[i])) {
+      count += 1
+    }
+    if (letterScores.twoPoints.includes(this.word[i])) {
+      count += 2
+    }
+    if (letterScores.threePoints.includes(this.word[i])) {
+      count += 3
+    }
+    if (letterScores.fourPoints.includes(this.word[i])) {
+      count += 4
+    }
+    if (letterScores.fivePoints.includes(this.word[i])) {
+      count += 5
+    }
+    if (letterScores.eightPoints.includes(this.word[i])) {
+      count += 8
+    }
+    if (letterScores.tenPoints.includes(this.word[i])) {
+      count += 10
+    }
+  }
+  return count
 }
 }
+scrabble = new Scrabble('wombat')
+
 
 module.exports = Scrabble

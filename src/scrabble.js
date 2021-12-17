1dbcdc846d86fd1c3f75a4f669fad9ef44df1565
Nico <nicolapicchio@gmail.com>
2021-12-17 14:51:37 +0000

    first commit

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,23 @@
 class Scrabble {
-  // Write your implementation here
+  constructor(word){
+    this.word = word
+  }
+  score() {
+  
+  this.word
+  }
+}
 
+
+
+const letterScores = {
+  onepoint: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  twopoints: ['D', 'G'],
+  threepoints: ['B', 'C', 'M', 'P'],
+  fourpoints: ['F', 'H', 'V', 'W', 'Y'],
+  fivepoints: ['K'],
+  sixpoints: ['J', 'X'],
+  sevenpoints: ['Q', 'Z']
 }
 
 module.exports = Scrabble

af2e20ef2306a3169fcd40b3f7d68e3387070425
marianphillips <79763160+marianphillips@users.noreply.github.com>
2021-12-17 15:33:40 +0000

    trimmed whitespace

--- src/scrabble.js
+++ src/scrabble.js
@@ -10,29 +10,31 @@ class Scrabble {
       return 0
     }
 
-    for (let i = 0; i < this.word.length; i++) {
-      if (this.word[i].match(/[^A-Z]/i)) {
+    const wordTrimmed = this.word.trim()
+
+    for (let i = 0; i < wordTrimmed.length; i++) {
+      if (wordTrimmed[i].match(/[^A-Z]/i)) {
         return 0
       }
-      if (this.word[i].match(/[AEIOULNRST]/i)) {
+      if (wordTrimmed[i].match(/[AEIOULNRST]/i)) {
         count++
       }
-      if (this.word[i].match(/[DG]/i)) {
+      if (wordTrimmed[i].match(/[DG]/i)) {
         count += 2
       }
-      if (this.word[i].match(/[BCMP]/i)) {
+      if (wordTrimmed[i].match(/[BCMP]/i)) {
         count += 3
       }
-      if (this.word[i].match(/[FHVWY]/i)) {
+      if (wordTrimmed[i].match(/[FHVWY]/i)) {
         count += 4
       }
-      if (this.word[i].match(/[K]/i)) {
+      if (wordTrimmed[i].match(/[K]/i)) {
         count += 5
       }
-      if (this.word[i].match(/[JX]/i)) {
+      if (wordTrimmed[i].match(/[JX]/i)) {
         count += 8
       }
-      if (this.word[i].match(/[QZ]/i)) {
+      if (wordTrimmed[i].match(/[QZ]/i)) {
         count += 10
       }
     }

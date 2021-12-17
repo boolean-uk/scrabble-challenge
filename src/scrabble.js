f80d33f15843c4f9447b2ea3b743570173fa16dc
Ben Corbin <ben@corbin.digital>
2021-12-17 15:13:49 +0000

    1st test passed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,10 +1,23 @@
+// const letterScores = {
+//   onepoint: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+//   twopoints: ['D', 'G'],
+//   threepoints: ['B', 'C', 'M', 'P'],
+//   fourpoints: ['F', 'H', 'V', 'W', 'Y'],
+//   fivepoints: ['K'],
+//   sixpoints: ['J', 'X'],
+//   sevenpoints: ['Q', 'Z']
+// } 
+
 class Scrabble {
   constructor(word){
     this.word = word
   }
 score() {
   //work out how to score here, probably with if statements
-  this.word
+  if (this.word.length === 0) {
+    return 0
+  }
+   
 }
 }
 

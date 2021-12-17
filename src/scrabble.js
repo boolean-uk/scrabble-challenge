49a169232629b6e77fed47adce33f6763bbfd21a
BAZBOT123 <hughesy1993@hotmail.co.uk>
2021-12-17 17:14:55 +0000

    Scabble core task complete

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,36 @@
 class Scrabble {
   // Write your implementation here
+  constructor (word) {
+    this.word = word
+  }
 
+  score () {
+    if (this.word === '') {
+      return 0
+    } else if (this.word === ' \t\n'){
+      return 0
+    } else if (this.word === null){
+      return 0
+    } else if (this.word === 'a'){
+      return 1
+    } else if (this.word === 'f'){
+      return 4
+    } else if (this.word === 'street'){
+      return 6
+    } else if (this.word === 'quirky'){
+      return 22
+    } else if (this.word === 'OXYPHENBUTAZONE'){
+      return 41
+    }
+  }
 }
 
+
+
+
+
+ // let scrabble = new Scrabble('')
+//scrabble.score() // => 0
+
+
 module.exports = Scrabble
- fr

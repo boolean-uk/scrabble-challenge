bc05876ed1ae77c8472d811314fd31ca0bf8f080
Paul Smart <91954372+therearedoors@users.noreply.github.com>
2021-12-19 19:32:43 +0000

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,22 +1,9 @@
 let boolean = true
 let points = 0
-const one = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
-const two = ['D', 'G']
-const three = ['B', 'C', 'M', 'P']
-const four = ['F', 'H', 'V', 'W', 'Y']
-const five = ['K']
-const eight = ['J', 'X']
-const ten = ['Q', 'Z']
+const letters = [["", " \t\n"]['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],['D', 'G'],['B', 'C', 'M', 'P'],['F', 'H', 'V', 'W', 'Y'],['K'],['J', 'X'],['Q', 'Z']]
 
 function noInput (type) {
-  type === null || type === ''
-    ? boolean = true
-    : boolean = false
-  return boolean
-}
-
-function nonsenseCheck (str) {
-  str.includes(' ')
+  type === null
     ? boolean = true
     : boolean = false
   return boolean
@@ -42,28 +29,16 @@ function scoreWord (arr) {
 }
 
 function scoreLetter (element) {
-  if (one.includes(element)) {
-    points += 1
-  }
-  if (two.includes(element)) {
-    points += 2
-  }
-  if (three.includes(element)) {
-    points += 3
-  }
-  if (four.includes(element)) {
-    points += 4
-  }
-  if (five.includes(element)) {
-    points += 5
-  }
-  if (eight.includes(element)) {
-    points += 8
-  }
-  if (ten.includes(element)) {
-    points += 10
-  }
-}
+for (let i=0;i<=7;i++){
+  if (i===6){if(letters[i].includes(element))
+                 {points += 8;}
+    }
+  if (i===7) {if(letters[i].includes(element)
+                 {points += 10;}
+    } else if (letters[i].includes(element))
+                 {points += i;}
+    }
+  
 class Scrabble {
   constructor (str) {
     this.str = str

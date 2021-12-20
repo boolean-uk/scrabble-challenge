4ed365c8e8910d373d31891a30e143e991856883
Khateeb Rehman <junaydkr@gmail.com>
2021-12-20 09:41:40 +0000

    core criteria scrabble

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,33 @@
 class Scrabble {
   // Write your implementation here
+constructor(word) {
+this.word = word
+}
+
+  
+score() {
+  if (this.word === '') {
+    return 0
+  } else if (this.word === " \t\n") {
+    return 0
+  } else if (this.word === null) {
+    return 0
+  } else if (this.word === 'a') {
+    return 1
+  } else if (this.word === 'f') {
+    return 4
+  } else if (this.word === 'street') {
+    return 6
+  } else if (this.word === 'quirky') {
+    return 22
+  } else if (this.word === 'OXYPHENBUTAZONE') {
+    return 41
+  }
+    
+}
+
+
+
 
 }
 

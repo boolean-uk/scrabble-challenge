0e90cf98c00fb0d0813df4e87358c93481fd7095
dan1212111 <danielmc122333@icloud.com>
2021-12-20 20:33:58 +0000

    finished

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,4 +1,4 @@
-const val1 = ["A", "E","I","O","U","L","N","R","S","T","a","e","i","o","u","l","n","r","s","t",];
+const val1 = ["A", "E","I","O","U","L","N","R","S","T","a","e","i","o","u","l","n","r","s","t"];
 const val2 = ["D", "d", "G", "g"];
 const val3 = ["B", "b", "C", "c", "M", "m", "P", "p"];
 const val4 = ["F", "f", "H", "h", "V", "v", "W", "w", "Y", "y"];
@@ -9,41 +9,48 @@ const val10 = ["Q", "q", "Z", "z"];
 class Scrabble {
   // Write your implementation here
 
-  construtor(word) {
+  constructor(word) {
     this.word = word;
+    console.log(this.word)
    }
 
   score() {
     let score = 0
 
-    if (this.word === undefined) {
+    if (this.word === undefined || this.word === null) {
       return 0
     }
 
     for (let i = 0; i < this.word.length; i++) {
       if (val1.includes(this.word[i])) {
-        score += 1
+        score += 1 
       }
       if (val2.includes(this.word[i])) {
-        score += 2
+        score += 2 
       }
       if (val3.includes(this.word[i])) {
-        score += 3
+        score += 3 
       }
       if (val4.includes(this.word[i])) {
-        score += 4
+        score += 4 
       }
       if (val5.includes(this.word[i])) {
-        score += 5
+        score += 5 
       }
       if (val8.includes(this.word[i])) {
-        score += 8
+        score += 8 
       }
       if (val10.includes(this.word[i])) {
-        score += 10
+        score += 10 
       }
     }
-    return score
+    return score 
+  }
+
+  bonus() {
+    let count = []
+
+   
   }
 }
 

140dad90e8e1e052a1105a6272923bac7028e34d
Paul Smart <pdsmart6@gmail.com>
2021-12-20 17:34:59 +0000

    Extension Submission

--- spec/scrabble-spec.js
+++ spec/scrabble-spec.js
@@ -1,77 +1,77 @@
 // Note: you shouldn't need to change anything in this file.
 
 Scrabble = require('../src/scrabble')
-let scrabble;
+let scrabble
 
-describe("Scrabble", function() {
-  it('returns 0 for empty words', function() {
+describe('Scrabble', function () {
+  it('returns 0 for empty words', function () {
     scrabble = new Scrabble('')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('returns 0 for whitespace', function() {
-    scrabble = new Scrabble(" \t\n")
+  it('returns 0 for whitespace', function () {
+    scrabble = new Scrabble(' \t\n')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('returns 0 for null', function() {
+  it('returns 0 for null', function () {
     scrabble = new Scrabble(null)
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('scores short word', function() {
+  it('scores short word', function () {
     scrabble = new Scrabble('a')
 
     expect(scrabble.score()).toEqual(1)
   })
 
-  it('scores short word', function() {
+  it('scores short word', function () {
     scrabble = new Scrabble('f')
 
     expect(scrabble.score()).toEqual(4)
   })
 
-  it('scores a simple word', function() {
+  it('scores a simple word', function () {
     scrabble = new Scrabble('street')
 
     expect(scrabble.score()).toEqual(6)
   })
 
-  it('scores a more complicated word', function() {
+  it('scores a more complicated word', function () {
     scrabble = new Scrabble('quirky')
 
     expect(scrabble.score()).toEqual(22)
   })
 
-  it('scores a case-insensitive word', function() {
+  it('scores a case-insensitive word', function () {
     scrabble = new Scrabble('OXYPHENBUTAZONE')
 
     expect(scrabble.score()).toEqual(41)
   })
 
-  it('scores a double letter', function() {
-    scrabble = new Scrabble("fis{h}")
+  it('scores a double letter', function () {
+    scrabble = new Scrabble('fis{h}')
 
     expect(scrabble.score()).toEqual(14)
   })
 
-  it('scores a triple letter', function() {
-    scrabble = new Scrabble("tri[p]le")
+  it('scores a triple letter', function () {
+    scrabble = new Scrabble('tri[p]le')
 
-    expect(scrabble.score()).toEqual(11)
+    expect(scrabble.score()).toEqual(14)
   })
 
-  it('scores a double word', function() {
-    scrabble = new Scrabble("{student}")
+  it('scores a double word', function () {
+    scrabble = new Scrabble('{student}')
 
     expect(scrabble.score()).toEqual(16)
   })
 
-  it('scores a triple word', function() {
-    scrabble = new Scrabble("[fox]")
+  it('scores a triple word', function () {
+    scrabble = new Scrabble('[fox]')
 
     expect(scrabble.score()).toEqual(39)
   })
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,20 +1,66 @@
-let boolean = true
+let doubleWord = false
+let doubleLetterRef = []
+let tripleWord = false
+let tripleLetterRef = []
 let points = 0
 const commonLetters = [['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], ['D', 'G'], ['B', 'C', 'M', 'P'], ['F', 'H', 'V', 'W', 'Y'], ['K']]
 const rareLetters = [['J', 'X'], ['Q', 'Z']]
+const double = /\{[A-Z]*\}/g
+const triple = /\[[A-Z]*\]/g
 
 function noInput (type) {
-  type === null
-    ? boolean = true
-    : boolean = false
-  return boolean
+  return type === null
+}
+
+function checkDouble (str, arr) {
+  return !!arr.includes(str)
+}
+
+function checkTriple (str, arr) {
+  return !!arr.includes(str)
+}
+
+function resetWordMultipliers () {
+  doubleWord = false
+  tripleWord = false
+}
+
+function resetLetterReferences () {
+  doubleLetterRef = []
+  tripleLetterRef = []
+}
+
+function logMultipliers (str) {
+  logDoubles(str)
+  logTriples(str)
+}
+
+function logDoubles (str) {
+  if (double.test(str)) {
+    doubleLetterRef = str.match(double)
+    doubleWord = checkDouble(str, str.match(double))
+  }
+}
+
+function logTriples (str) {
+  if (triple.test(str)) {
+    tripleLetterRef = str.match(triple)
+    tripleWord = checkTriple(str, str.match(triple))
+  }
 }
 
 function parseWord (str) {
+  resetWordMultipliers()
+  logMultipliers(str)
+  return stringToArray(str)
+}
+
+function stringToArray (str) {
   const arr = []
   for (let i = 0; i < str.length; i++) {
     arr.push(str.charAt(i).toUpperCase())
-  } return arr
+  }
+  return arr
 }
 
 function resetPoints () {
@@ -26,20 +72,46 @@ function scoreWord (arr) {
   for (let i = 0; i < arr.length; i++) {
     scoreLetter(arr[i])
   }
-  return points
+  resetLetterReferences()
+  scrabbleWorld()
+}
+
+function scrabbleWorld () {
+  if (doubleWord) {
+    points *= 2
+  }
+  if (tripleWord) {
+    points *= 3
+  }
+}
+
+function scoreCommonLetter (element) {
+  for (let i = 0; i < 5; i++) {
+    if (commonLetters[i].includes(element)) {
+      scrabbleLetter(element, i + 1)
+    }
+  }
+}
+
+function scoreRareLetter (element) {
+  for (let i = 0; i < 2; i++) {
+    if (rareLetters[i].includes(element)) {
+      scrabbleLetter(element, (i + 4) * 2)
+    }
+  }
+}
+
+function scrabbleLetter (element, pts) {
+  points += checkDouble(`{${element}}`, doubleLetterRef)
+    ? pts * 2
+    : checkTriple(`[${element}]`, tripleLetterRef)
+      ? pts * 3
+      : pts
 }
 
 function scoreLetter (element) {
-  for (let i = 0; i < 5; i++) {
-    if (commonLetters[i].includes(element)) {
-      points += i + 1
-    }
-  }
-  for (let i = 0; i < 2; i++) {
-    if (rareLetters[i].includes(element)) {
-      points += (i + 4) * 2
-    }
-  }
+  scoreCommonLetter(element)
+  scoreRareLetter(element)
 }
 
 class Scrabble {
@@ -51,7 +123,10 @@ class Scrabble {
   score () {
     if (noInput(this.str)) {
       points = 0
-    } else scoreWord(parseWord(this.str))
+    } else {
+      const str = this.str.toUpperCase()
+      scoreWord(parseWord(str))
+    }
     return points
   }
 }

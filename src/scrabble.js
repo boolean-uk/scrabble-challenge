0df08d209576df75d131c80f519a8d34ce95a00b
Paul Smart <pdsmart6@gmail.com>
2021-12-20 20:03:09 +0000

    Organised Functions + commented code

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,3 @@
-/* [^\{\}] */
-/* [^\[\]] */
 let doubleWord = false
 let doubleLetterRef = []
 let tripleWord = false
@@ -10,63 +8,15 @@ const rareLetters = [['J', 'X'], ['Q', 'Z']]
 const double = /\{[A-Z{}[\]]*\}/g
 const triple = /\[[A-Z{}[\]]*\]/g
 
+// functions called directly by Scrabble class
 function noInput (type) {
   return type === null
-}
-
-function checkDouble (str, arr) {
-  return arr.includes(str)
-}
-
-function checkTriple (str, arr) {
-  return arr.includes(str)
-}
-
-function resetWordMultipliers () {
-  doubleWord = false
-  tripleWord = false
-}
-
-function resetLetterReferences () {
-  doubleLetterRef = []
-  tripleLetterRef = []
-}
-
-function logMultipliers (str) {
-  logDoubles(str)
-  logTriples(str)
-}
-
-function logDoubles (str) {
-  if (double.test(str)) {
-    doubleLetterRef = str.match(double)
-    doubleWord = checkDouble(str, str.match(double))
-  }
-}
-
-function logTriples (str) {
-  if (triple.test(str)) {
-    tripleLetterRef = str.match(triple)
-    tripleWord = checkTriple(str, str.match(triple))
-  }
 }
 
 function parseWord (str) {
   resetWordMultipliers()
   logMultipliers(str)
   return stringToArray(str)
-}
-
-function stringToArray (str) {
-  const arr = []
-  for (let i = 0; i < str.length; i++) {
-    arr.push(str.charAt(i).toUpperCase())
-  }
-  return arr
-}
-
-function resetPoints () {
-  points = 0
 }
 
 function scoreWord (arr) {
@@ -77,6 +27,38 @@ function scoreWord (arr) {
   resetLetterReferences()
   scrabbleWord()
 }
+// functions called by parseWord
+function resetWordMultipliers () {
+  doubleWord = false
+  tripleWord = false
+}
+
+function logMultipliers (str) {
+  logDoubles(str) // see below lines 72-84, broke up this function for concision
+  logTriples(str)
+}
+
+function stringToArray (str) {
+  const arr = []
+  for (let i = 0; i < str.length; i++) {
+    arr.push(str.charAt(i).toUpperCase())
+  }
+  return arr
+}
+// functions called by scoreWord
+function resetPoints () {
+  points = 0
+}
+
+function scoreLetter (element) {
+  scoreCommonLetter(element) // below lines 86-100 - broke up for concision
+  scoreRareLetter(element)
+}
+
+function resetLetterReferences () {
+  doubleLetterRef = []
+  tripleLetterRef = []
+}
 
 function scrabbleWord () {
   if (doubleWord) {
@@ -86,8 +68,22 @@ function scrabbleWord () {
     points *= 3
   }
 }
+// functions called by logMultipliers
+function logDoubles (str) {
+  if (double.test(str)) { // updates variables that track double status of words and letters
+    doubleLetterRef = str.match(double) // double letter statuses stored by array
+    doubleWord = checkDouble(str, str.match(double)) // double word status stored by boolean
+  }
+}
 
-function scoreCommonLetter (element) {
+function logTriples (str) {
+  if (triple.test(str)) { // same as with doubles for triples
+    tripleLetterRef = str.match(triple)
+    tripleWord = checkTriple(str, str.match(triple))
+  }
+}
+// functions called by scoreLetter
+function scoreCommonLetter (element) { // "common" letter scores from 1-5
   for (let i = 0; i < 5; i++) {
     if (commonLetters[i].includes(element)) {
       scrabbleLetter(element, i + 1)
@@ -96,24 +92,27 @@ function scoreCommonLetter (element) {
 }
 
 function scoreRareLetter (element) {
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 2; i++) { // "rare" letter scores 8 and 10
     if (rareLetters[i].includes(element)) {
       scrabbleLetter(element, (i + 4) * 2)
     }
   }
 }
-
+// function called by scoreCommonLetter and scoreRareLetter
 function scrabbleLetter (element, pts) {
-  points += checkDouble(`{${element}}`, doubleLetterRef)
+  points += checkDouble(`{${element}}`, doubleLetterRef) // checks whether double or triple or neither and updates points accordingly
     ? pts * 2
     : checkTriple(`[${element}]`, tripleLetterRef)
       ? pts * 3
       : pts
 }
+// functions called by logDoubles, logTriples and scrabbleLetter
+function checkDouble (str, arr) {
+  return arr.includes(str) // created this pair of functions for concision, adds readability
+}
 
-function scoreLetter (element) {
-  scoreCommonLetter(element)
-  scoreRareLetter(element)
+function checkTriple (str, arr) {
+  return arr.includes(str)
 }
 
 class Scrabble {

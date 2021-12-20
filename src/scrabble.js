7035e8dae58de80d6b7e46a008c9348a14244197
omar <omaralsoyouf@gmail.com>
2021-12-20 15:46:01 +0000

    omar submission

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,28 +1,55 @@
-class Scrabble {
-  constructor(word) {
-    this.word = word;
-  }
+const POINTS = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4, K: 5,
+  J: 8, X: 8, Q: 10, Z: 10, '}': 0, '{': 0, '[': 0, ']': 0
+};
 
+class Scrabble {
+  constructor(string) {
+    this.string = string
+  }
   score() {
-    let sum = 0;
-    if (this.word == null) {
-      return 0;
+    let sum = 0
+
+    if (this.string === null) {
+      return 0
     }
-    for (let i = 0; i < this.word.length; i++) {
-      if (/[a-zA-Z]/.test(this.word[i])) {
-        sum += POINTS[indexing(this.word[i])];
+
+    let word = this.string.toUpperCase()
+
+    for (let n = 0; n < word.length; n++) {
+      if (/[A-Z]/.test(word[n])) {
+        sum += POINTS[word[n]]
+      }
+      else if (word[0] === '{' && word[word.length - 1] === '}') {
+        return double(word)
+      }
+      else if (word[0] === '[' && word[word.length - 1] === ']') {
+        return triple(word)
+      }
+      else if (word[n] == '{') {
+        sum += POINTS[word[n + 1]]
+      }
+      else if (word[n] == '[') {
+        sum += POINTS[word[n + 1]] * 2
       }
     }
     return sum;
   }
 }
-let POINTS = [
-  1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4,
-  10,
-];
-function indexing(char) {
-  let lastChar = char.toLowerCase();
-  return lastChar.charCodeAt(0) - "a".charCodeAt(0);
+function double(word) {
+  let sum = 0;
+  for (let n of word) {
+    sum += POINTS[n] * 2
+  }
+  return sum;
+}
+function triple(word) {
+  let sum = 0;
+  for (let n of word) {
+    sum += POINTS[n] * 3
+  }
+  return sum;
 }
 
-module.exports = Scrabble;
+module.exports = Scrabble

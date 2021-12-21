2288a94420aa357614e9421aec37c05b0b89dcf2
George T <n35.nnlpd@slmail.me>
2021-12-21 09:51:31 +0000

    519

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,19 @@
+let letterValues = {
+
+  1:  "AEIOULNRST",
+  2:  "DG",
+  3:  "BCMP",
+  4:  "FHVWY",
+  5:  "K",
+  8:  "JX",
+  10: "QZ"
+};
+
 class Scrabble {
   // Write your implementation here
-
+  score(word) {
+  
+  }
 }
 
 module.exports = Scrabble

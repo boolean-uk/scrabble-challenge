bee2df656895caf29ab394ffbf774d9edc7ae0b9
George T <n35.nnlpd@slmail.me>
2022-01-06 17:14:29 +0000

    Initial commit

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,14 +1,3 @@
-let letterValues = {
-
-  1:  "AEIOULNRST",
-  2:  "DG",
-  3:  "BCMP",
-  4:  "FHVWY",
-  5:  "K",
-  8:  "JX",
-  10: "QZ"
-};
-
 class Scrabble {
   // Write your implementation here
   score(word) {

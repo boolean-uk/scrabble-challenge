9ef736ab65262f110c3ca301e27239272dae7e71
George T <n35.nnlpd@slmail.me>
2022-01-11 18:04:47 +0000

    passes 5 out of 8 tests

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,28 @@
 class Scrabble {
   // Write your implementation here
-  score(word) {
-  
+  constructor(word) {
+    this.word = word;
+  }
+
+  score() {
+    let points = 0;
+    let one = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
+
+    if (this.word === "") {
+      return 0;
+    } else if (this.word === null) {
+      return 0;
+    } else if (this.word === " \t\n") {
+      return 0;
+    } else {
+      for (let i = 0; i < this.word.length; i++) {
+        if (one.includes(this.word[i])) {
+          ++points;
+        }
+      }
+      return points;
+    }
   }
 }
 
-module.exports = Scrabble
+module.exports = Scrabble;

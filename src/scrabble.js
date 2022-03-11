0a3e7345c16ed6f253079b232e84a7a4dfd11cee
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-03-11 17:56:30 +0100

    first commit

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,32 @@
 class Scrabble {
   // Write your implementation here
-
+  constructor () {
+    return true
+  }
+/*
+  ScrabbleScore (word) {
+    for (let i = 0; i = word.length; i++) {
+      const letters = word[i]
+      if (letters = 'a' || 'e' || 'i' || 'o' || 'u' || 'l' || 'n' || 'r' || 's' || 't') {
+        return scrabbleScore += 1
+      } else if (letters = 'd' || 'g') {
+        return scrabbleScore += 2
+      } else if (letters = 'b' || 'c' || 'm' || 'p') {
+        return scrabbleScore += 3
+      } else if (letters = 'f' || 'h' || 'v' || 'w' || 'y') {
+        return scrabbleScore += 4
+      } else if (letters = 'k') {
+        return scrabbleScore += 5
+      } else if (letters = 'j' || 'x') {
+        return scrabbleScore += 8
+      } else if (letters = 'q' || 'z') {
+        scrabbleScore += 10
+      } else {
+        scrabbleScore += 0
+      }
+    }
+    return scrabbleScore
+  }
+  */
 }
-
 module.exports = Scrabble

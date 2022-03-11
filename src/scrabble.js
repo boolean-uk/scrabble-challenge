db6d83f00ae7282d5a5ff8867a2a62e5cebb3221
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-11 16:23:38 +0100

    FT: Basic Scrabble

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,35 @@
-class Scrabble {
-  // Write your implementation here
+const ScoreUtil = require('./utils/Scores')
 
+class Scrabble {
+  constructor (word) {
+    this.word = word
+  }
+
+  isValid () {
+    return (
+      this.word != null && typeof this.word === 'string' && this.word.length > 0
+    )
+  }
+
+  getCharScore (char) {
+    if (ScoreUtil.scores[char]) return ScoreUtil.scores[char]
+    else return 0
+  }
+
+  score () {
+    let s = 0
+    if (!this.isValid()) return s
+    this.word
+      .replace(' ', '')
+      .toLowerCase()
+      .split('')
+      .forEach((ch) => (s += this.getCharScore(ch)))
+
+    return s
+  }
 }
+
+const t = new Scrabble('    \n')
+console.log(t.score())
 
 module.exports = Scrabble

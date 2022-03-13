48e64a96688eb2a0aadd7c499a9f8617657b9acc
Chris Hyde <c_hyde333@hotmail.com>
2022-03-13 21:51:22 +0000

    latest commit

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,9 +1,46 @@
 // Declare the class:
 class Scrabble {
   // Write your implementation here
-  constructor () {
-    this.test = 0
+  constructor(letters) {
+    this.letters = letters;
+  }
+
+  score(valueOFLetter) {
+    // let total to 0
+    // Do a for loop for each character of this.letters
+    // add value of each letter to total
+    // finally return total
+    return 0;
+  }
+
+  valueOFLetter(character) {
+    if ("AEIOULNRST".indexOf(character) !== -1) {
+      return 1;
+    }
+    if ("DG".indexOf(character) !== -1) {
+      return 2;
+    }
+
+    if (((char = A), E, I, O, U, L, N, R, S, T)) return 1;
+    else {
+      if (((char = D), G)) return 2;
+      else {
+        if (((char = B), C, M, P)) return 3;
+        else {
+          if (((char = F), H, V, W, Y)) return 4;
+          else {
+            if ((char = K)) return 5;
+            else {
+              if (((char = J), X)) return 8;
+              else {
+                if (((char = Q), Z)) return 10;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 
-module.exports = Scrabble
+module.exports = Scrabble;

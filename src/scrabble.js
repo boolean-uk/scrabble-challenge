056e7a3cd1425d90b875cc8b4d630c3fe5c14560
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-13 22:34:15 +0000

    Beginning

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,9 @@
 class Scrabble {
   // Write your implementation here
 
+  // For this challenge use CLASSES, FUNCTIONS, LOOPS and FLOW CONTROL.
 }
 
 module.exports = Scrabble
+
+// ./node_modules/jasmine/bin/jasmine.js

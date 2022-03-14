8cac7019435b986d67745445619d9afdb67e206b
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-14 18:43:31 +0000

    ext

--- spec/scrabble-spec.js
+++ spec/scrabble-spec.js
@@ -3,74 +3,74 @@
 scrabble = require("../src/scrabble");
 let scrabble;
 
-describe("Scrabble", function () {
-  it("returns 0 for empty words", function () {
+describe("Scrabble", function() {
+  it("returns 0 for empty words", function() {
     scrabble = new Scrabble("");
 
     expect(scrabble.score()).toEqual(0);
   });
 
-  it("returns 0 for whitespace", function () {
+  it("returns 0 for whitespace", function() {
     scrabble = new Scrabble(" \t\n");
 
     expect(scrabble.score()).toEqual(0);
   });
 
-  it("returns 0 for null", function () {
+  it("returns 0 for null", function() {
     scrabble = new Scrabble(null);
 
     expect(scrabble.score()).toEqual(0);
   });
 
-  it("scores short word", function () {
+  it("scores short word", function() {
     scrabble = new Scrabble("a");
 
     expect(scrabble.score()).toEqual(1);
   });
 
-  it("scores short word", function () {
+  it("scores short word", function() {
     scrabble = new Scrabble("f");
 
     expect(scrabble.score()).toEqual(4);
   });
 
-  it("scores a simple word", function () {
+  it("scores a simple word", function() {
     scrabble = new Scrabble("street");
 
     expect(scrabble.score()).toEqual(6);
   });
 
-  it("scores a more complicated word", function () {
+  it("scores a more complicated word", function() {
     scrabble = new Scrabble("quirky");
 
     expect(scrabble.score()).toEqual(22);
   });
 
-  it("scores a case-insensitive word", function () {
+  it("scores a case-insensitive word", function() {
     scrabble = new Scrabble("OXYPHENBUTAZONE");
 
     expect(scrabble.score()).toEqual(41);
   });
 
-  it("scores a word with double points if {}", function () {
+  it("scores a word with double points if {}", function() {
     scrabble = new Scrabble("{dog}");
 
     expect(scrabble.score()).toEqual(10);
   });
 
-  it("scores a word with triple points if []", function () {
+  it("scores a word with triple points if []", function() {
     scrabble = new Scrabble("[dog]");
 
     expect(scrabble.score()).toEqual(15);
   });
 
-  it("scores a letter with double points if {}", function () {
+  it("scores a letter with double points if {}", function() {
     scrabble = new Scrabble("d{o}g");
 
     expect(scrabble.score()).toEqual(6);
   });
 
-  it("scores a letter with triple points if []", function () {
+  it("scores a letter with triple points if []", function() {
     scrabble = new Scrabble("d[o]g");
 
     expect(scrabble.score()).toEqual(7);

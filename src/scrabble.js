7d7d8da084caf84be8da16c9f4ff033ed10baa43
Chris Hyde <c_hyde333@hotmail.com>
2022-03-14 15:37:34 +0000

    Completed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,46 +1,89 @@
+
+const lettersScore = {
+  'a': 1,
+  'b': 3,
+  'c': 3,
+  'd': 2,
+  'e': 1,
+  'f': 4,
+  'g': 2,
+  'h': 4,
+  'i': 1,
+  'j': 8,
+  'k': 5,
+  'l': 1,
+  'm': 3,
+  'n': 1,
+  'o': 1,
+  'p': 3,
+  'q': 10,
+  'r': 1,
+  's': 1,
+  't': 1,
+  'u': 1,
+  'v': 4,
+  'w': 4,
+  'x': 8,
+  'y': 4,
+  'z': 10
+}
+
 // Declare the class:
 class Scrabble {
   // Write your implementation here
-  constructor(letters) {
-    this.letters = letters;
+  constructor (letters) {
+    this.letters = letters
   }
 
-  score(valueOFLetter) {
+  score () {
     // let total to 0
+    let totalScore = 0
+    if (this.letters === null) {
+      return totalScore
+    }
+    const letters = this.letters.toLowerCase() 
+
     // Do a for loop for each character of this.letters
-    // add value of each letter to total
-    // finally return total
-    return 0;
-  }
-
-  valueOFLetter(character) {
-    if ("AEIOULNRST".indexOf(character) !== -1) {
-      return 1;
-    }
-    if ("DG".indexOf(character) !== -1) {
-      return 2;
-    }
-
-    if (((char = A), E, I, O, U, L, N, R, S, T)) return 1;
-    else {
-      if (((char = D), G)) return 2;
-      else {
-        if (((char = B), C, M, P)) return 3;
-        else {
-          if (((char = F), H, V, W, Y)) return 4;
-          else {
-            if ((char = K)) return 5;
-            else {
-              if (((char = J), X)) return 8;
-              else {
-                if (((char = Q), Z)) return 10;
-              }
-            }
-          }
-        }
-      }
-    }
+    for (let i = 0; i < letters.length; ++i) {
+      // add value of each letter to total
+      totalScore += lettersScore[letters.charAt(i)] || 0;
+      // finally return total
+    } 
+    return totalScore 
   }
 }
 
-module.exports = Scrabble;
+// Previous work before Ed's introduction to tackle the task
+
+//   valueOFLetter (character) {
+//     if ('AEIOULNRST'.indexOf(character) !== -1) {
+//       return 1
+//     }
+//     if ('DG'.indexOf(character) !== -1) {
+//       return 2
+//     }
+
+//     if (char = AEIOULNRST) return 1
+//     else {
+//       if (((char = D), G)) return 2
+//       else {
+//         if (((char = B), C, M, P)) return 3
+//         else {
+//           if (((char = F), H, V, W, Y)) return 4
+//           else {
+//             if ((char = K)) return 5
+//             else {
+//               if (((char = J), X)) return 8
+//               else {
+//                 if (((char = Q), Z)) return 10
+//   }
+// }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+module.exports = Scrabble

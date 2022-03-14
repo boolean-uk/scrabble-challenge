7aa867c510699a8a031cd56f91fa980928821fc6
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-14 18:42:11 +0000

    ext

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,34 +1,86 @@
+const letterScores = {
+  onePoint: [
+    "a",
+    "A",
+    "e",
+    "E",
+    "i",
+    "I",
+    "o",
+    "O",
+    "u",
+    "U",
+    "l",
+    "L",
+    "n",
+    "N",
+    "r",
+    "R",
+    "s",
+    "S",
+    "t",
+    "T",
+  ],
+  twoPoints: ["d", "D", "g", "G"],
+  threePoints: ["b", "B", "c", "C", "m", "M", "p", "P"],
+  fourPoints: ["f", "F", "h", "H", "v", "V", "w", "W", "y", "Y"],
+  fivePoints: ["k", "K"],
+  eightPoints: ["j", "J", "x", "X"],
+  tenPoints: ["q", "Q", "z", "Z"],
+};
+
 class Scrabble {
   constructor(word) {
     this.word = word;
-    this.onePoint = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
-    this.twoPoints = ["d", "g"];
-    this.threePoints = ["b", "c", "m", "p"];
-    this.fourPoints = ["f", "h", "v", "w", "y"];
-    this.fivePoints = ["k"];
-    this.eightPoints = ["j", "x"];
-    this.tenPoints = ["q", "z"];
   }
-
   score() {
-    let score = 0;
-    if (typeof this.word !== "string") {
-      return score;
-    } else {
-      const wordArr = this.word.toLowerCase().split("");
-      wordArr.filter((letter) => {
-        if (this.onePoint.includes(letter)) score += 1;
-        if (this.twoPoints.includes(letter)) score += 2;
-        if (this.threePoints.includes(letter)) score += 3;
-        if (this.fourPoints.includes(letter)) score += 4;
-        if (this.fivePoints.includes(letter)) score += 5;
-        if (this.eightPoints.includes(letter)) score += 8;
-        if (this.tenPoints.includes(letter)) score += 10;
-        return score;
-      });
+    let count = 0;
+    if (this.word === null || this.word.length === 0 || this.word.trim === "") {
+      return count;
     }
-    return score;
+    for (let i = 0; i < this.word.length; i++) {
+      if (letterScores.onePoint.includes(this.word[i])) count += 1;
+      if (letterScores.twoPoints.includes(this.word[i])) count += 2;
+      if (letterScores.threePoints.includes(this.word[i])) count += 3;
+      if (letterScores.fourPoints.includes(this.word[i])) count += 4;
+      if (letterScores.fivePoints.includes(this.word[i])) count += 5;
+      if (letterScores.eightPoints.includes(this.word[i])) count += 8;
+      if (letterScores.tenPoints.includes(this.word[i])) count += 10;
+    }
+    for (let j = 0, jl = count; j < this.word.length; j++) {
+      if (this.word[j] === "{" && this.word[j + 2] === "}") {
+        if (letterScores.onePoint.includes(this.word[j + 1])) count += 1;
+        if (letterScores.twoPoints.includes(this.word[j + 1])) count += 2;
+        if (letterScores.threePoints.includes(this.word[j + 1])) count += 3;
+        if (letterScores.fourPoints.includes(this.word[j + 1])) count += 4;
+        if (letterScores.fivePoints.includes(this.word[j + 1])) count += 5;
+        if (letterScores.eightPoints.includes(this.word[j + 1])) count += 8;
+        if (letterScores.tenPoints.includes(this.word[j + 1])) count += 10;
+      }
+      if (this.word[j] === "[" && this.word[j + 2] === "]") {
+        if (letterScores.onePoint.includes(this.word[j + 1])) count += 2;
+        if (letterScores.twoPoints.includes(this.word[j + 1])) count += 4;
+        if (letterScores.threePoints.includes(this.word[j + 1])) count += 6;
+        if (letterScores.fourPoints.includes(this.word[j + 1])) count += 8;
+        if (letterScores.fivePoints.includes(this.word[j + 1])) count += 10;
+        if (letterScores.eightPoints.includes(this.word[j + 1])) count += 16;
+        if (letterScores.tenPoints.includes(this.word[j + 1])) count += 20;
+      }
+    }
+    if (
+      this.word.charAt(0) === "{" &&
+      this.word.charAt(this.word.length - 1) === "}"
+    )
+      count *= 2;
+    if (
+      this.word.charAt(0) === "[" &&
+      this.word.charAt(this.word.length - 1) === "]"
+    )
+      count *= 3;
+    return count;
   }
 }
+
+Scrabble = new Scrabble("dog");
 
 module.exports = Scrabble;

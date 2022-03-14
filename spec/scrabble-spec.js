fa51d5a8cce2df6b05864cd1fd2dfa2dbe7b0b3f
David <czdavid93@gmail.com>
2022-03-14 12:26:48 +0100

    double, triple letters work

--- spec/scrabble-spec.js
+++ spec/scrabble-spec.js
@@ -1,61 +1,94 @@
 // Note: you shouldn't need to change anything in this file.
 
 Scrabble = require('../src/scrabble')
-let scrabble;
+let scrabble
 
-describe("Scrabble", function() {
-  it('returns 0 for empty words', function() {
+describe('Scrabble', function () {
+  it('returns 0 for empty words', function () {
     scrabble = new Scrabble('')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-
-  it('returns 0 for whitespace', function() {
-    scrabble = new Scrabble(" \t\n")
+  it('returns 0 for whitespace', function () {
+    scrabble = new Scrabble(' \t\n')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-
-  it('returns 0 for null', function() {
+  it('returns 0 for null', function () {
     scrabble = new Scrabble(null)
 
     expect(scrabble.score()).toEqual(0)
   })
 
-
-  it('scores short word', function() {
+  it('scores short word', function () {
     scrabble = new Scrabble('a')
 
     expect(scrabble.score()).toEqual(1)
   })
 
-
-  it('scores short word', function() {
+  it('scores short word', function () {
     scrabble = new Scrabble('f')
 
     expect(scrabble.score()).toEqual(4)
   })
 
-
-  it('scores a simple word', function() {
+  it('scores a simple word', function () {
     scrabble = new Scrabble('street')
 
     expect(scrabble.score()).toEqual(6)
   })
 
-
-  it('scores a more complicated word', function() {
+  it('scores a more complicated word', function () {
     scrabble = new Scrabble('quirky')
 
     expect(scrabble.score()).toEqual(22)
   })
 
-  
-  it('scores a case-insensitive word', function() {
+  it('scores a case-insensitive word', function () {
     scrabble = new Scrabble('OXYPHENBUTAZONE')
 
     expect(scrabble.score()).toEqual(41)
   })
+
+  // -------extension tests added below-----------
+
+  it('short word with double points', function () {
+    scrabble = new Scrabble('{a}')
+
+    expect(scrabble.score()).toEqual(2)
+  })
+
+  it('short word with triple points', function () {
+    scrabble = new Scrabble('[j]')
+
+    expect(scrabble.score()).toEqual(24)
+  })
+
+  it('case-sensitive longer word with doulbe and triple letter', function () {
+    scrabble = new Scrabble('Su{p}er[m]an')
+
+    expect(scrabble.score()).toEqual(21)
+  })
+
+  it('short word with double letter', function () {
+    scrabble = new Scrabble('{a}ei')
+
+    expect(scrabble.score()).toEqual(4)
+  })
+
+  // ---------extension tests for double and tripple words ------------
+
+  //   it('double word', function() {
+  //     scrabble = new Scrabble('{Knife}')
+
+  //     expect(scrabble.score()).toEqual(24)
+  //   })
+
+  //   it('triple word', function() {
+  //     scrabble = new Scrabble('[XRAY]')
+
+  //     expect(scrabble.score()).toEqual(28)
+  //   })
 })
--- src/scrabble.js
+++ src/scrabble.js
@@ -4,12 +4,12 @@ class Scrabble {
     this.points = 0
   }
 
-  score (word) {
+  // first find out if the string is empty string, whitepsace or null, then score 0.
+  score () {
     if (this.word === '' || this.word === ' ') {
       this.points += 0
       return 0
     }
-
     if (this.word === ' \t\n') {
       this.points += 0
       return 0
@@ -19,8 +19,9 @@ class Scrabble {
       this.points += 0
       return 0
     }
-
+    // I use for loop on the string to score each letter on each iteration
     for (let i = 0; i < this.word.length; i++) {
+      // thise are the letters in the corresponting arrays (based on the points value), stored in a variable
       const pointOne = ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
       const pointTwo = ['d', 'g']
       const pointThree = ['b', 'c', 'm', 'p']
@@ -28,42 +29,105 @@ class Scrabble {
       const pointFive = ['k']
       const pointEight = ['j', 'x']
       const pointTen = ['q', 'z']
+      // i declared variables for the double/triple points, it can be either True or False
+      let doublePoints = false
+      let triplePoints = false
+      // if the actual character in the string in this actual iteration of the loop is "{" or "[" => then double points will switch to True AND i++ so we jump on the next letter in the string and use IF statements to find out if that letter is TRUE, FALSE or else.
+      if (this.word[i] === '{') {
+        doublePoints = true
+        i++
+      }
+      // if the character is "}" or "]" then double points will swithc to FALSE therefore the letter after this character only worth single points.
+      if (this.word[i] === '}') {
+        doublePoints = false
+      }
+
+      if (this.word[i] === '[') {
+        triplePoints = true
+        i++
+      }
+
+      if (this.word[i] === ']') {
+        triplePoints = false
+      }
 
       if (
         pointOne.includes(this.word[i]) ||
         pointOne.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 1
+        if (doublePoints === true) {
+          this.points += 1 * 2
+        } else if (triplePoints === true) {
+          this.points += 1 * 3
+        } else {
+          this.points += 1
+        }
       } else if (
         pointTwo.includes(this.word[i]) ||
         pointTwo.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 2
+        if (doublePoints === true) {
+          this.points += 2 * 2
+        } else if (triplePoints === true) {
+          this.points += 2 * 3
+        } else {
+          this.points += 2
+        }
       } else if (
         pointThree.includes(this.word[i]) ||
         pointThree.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 3
+        if (doublePoints === true) {
+          this.points += 3 * 2
+        } else if (triplePoints === true) {
+          this.points += 3 * 3
+        } else {
+          this.points += 3
+        }
       } else if (
         pointFour.includes(this.word[i]) ||
         pointFour.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 4
+        if (doublePoints === true) {
+          this.points += 4 * 2
+        } else if (triplePoints === true) {
+          this.points += 4 * 3
+        } else {
+          this.points += 4
+        }
       } else if (
         pointFive.includes(this.word[i]) ||
         pointFive.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 5
+        if (doublePoints === true) {
+          this.points += 5 * 2
+        } else if (triplePoints === true) {
+          this.points += 5 * 3
+        } else {
+          this.points += 5
+        }
       } else if (
         pointEight.includes(this.word[i]) ||
         pointEight.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 8
+        if (doublePoints === true) {
+          this.points += 8 * 2
+        } else if (triplePoints === true) {
+          this.points += 8 * 3
+        } else {
+          this.points += 8
+        }
       } else if (
         pointTen.includes(this.word[i]) ||
         pointTen.includes(this.word[i].toLowerCase())
       ) {
-        this.points += 10
+        if (doublePoints === true) {
+          this.points += 10 * 2
+        } else if (triplePoints === true) {
+          this.points += 10 * 3
+        } else {
+          this.points += 10
+        }
       }
     }
     return this.points

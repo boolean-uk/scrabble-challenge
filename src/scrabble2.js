eec505588c716b966bd97d6c79e051da3de2f978
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-15 18:13:23 +0000

    Scrabble 2 practise purposes

---
+++ src/scrabble2.js
@@ -0,0 +1,6 @@
+// PSEUDO CODE
+// 1. You want an object and arrays inside it with all the given letters/alphabets with their own scores.
+// 2. A class, constructor and this.word
+// 3. You want 2 or 3 paragraphs about if not a string (!= this.word), (this.word = ('\t) || this.word = ('/n)). For all of these 3 return 0.
+// 4. A loop that generates all the letters/alphabets
+// 5. return a number based on what happened inside the loop. So return += score.

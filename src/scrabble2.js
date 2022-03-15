cce15232e8625332ba323721ee86a307f375b0c7
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-15 21:04:44 +0000

    Scrabble 2 first attempt

--- src/scrabble2.js
+++ src/scrabble2.js
@@ -4,3 +4,35 @@
 // 3. You want 2 or 3 paragraphs about if not a string (!= this.word), (this.word = ('\t) || this.word = ('/n)). For all of these 3 return 0.
 // 4. A loop that generates all the letters/alphabets
 // 5. return a number based on what happened inside the loop. So return += score.
+
+const letters = {
+    value1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
+    value2 = ["D", "G"];
+    value3 = ["B", "C", "M", "P"];
+    value4 = ["F", "H", "V", "W", "Y"];
+    value5 = ["K"];
+    value8 = ["J", "X"];
+    value10 = ["Q", "Z"];
+}
+
+class Scrabble {
+    constructor (word) {
+        this.word = word;
+    }
+
+    score () {
+
+    if (!= this.word)
+    return 0
+
+    if (this.word = ('\t' || this.word = ('/n'))) 
+    return 0
+
+    for (let i = 0; i < this.word; i++)
+    this.word[i];
+
+    score += letters;
+}
+}
+
+

6780764e5ba343078d9059d6fda9e5d6841539d1
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-15 17:54:06 +0100

    removed .split() bc there is no need to convert string to array

--- src/scrabble.js
+++ src/scrabble.js
@@ -18,27 +18,26 @@ class Scrabble {
     if (typeof this.word !== 'string') {
       return finalScore
     } else {
-      const wordArr = this.word.toLowerCase().split('')
-
-      for (let i = 0; i < wordArr.length; i++) {
+      const lowerCasedWord = this.word.toLowerCase()
+      for (let i = 0; i < lowerCasedWord.length; i++) {
         // check if there is curly bracket or square bracket
         // if so, change the multiply to 2 or 3. If not, multiply remains 1
-        if (wordArr[i] === '{') {
+        if (lowerCasedWord[i] === '{') {
           multiply = multiply * 2
-        } else if (wordArr[i] === '}') {
+        } else if (lowerCasedWord[i] === '}') {
           multiply = multiply / 2
-        } else if (wordArr[i] === '[') {
+        } else if (lowerCasedWord[i] === '[') {
           multiply = multiply * 3
-        } else if (wordArr[i] === ']') {
+        } else if (lowerCasedWord[i] === ']') {
           multiply = multiply / 3
         } else {
-          if (this.onePoint.includes(wordArr[i])) subScore = 1
-          if (this.twoPoints.includes(wordArr[i])) subScore = 2
-          if (this.threePoints.includes(wordArr[i])) subScore = 3
-          if (this.fourPoints.includes(wordArr[i])) subScore = 4
-          if (this.fivePoints.includes(wordArr[i])) subScore = 5
-          if (this.eightPoints.includes(wordArr[i])) subScore = 8
-          if (this.tenPoints.includes(wordArr[i])) subScore = 10
+          if (this.onePoint.includes(lowerCasedWord[i])) subScore = 1
+          if (this.twoPoints.includes(lowerCasedWord[i])) subScore = 2
+          if (this.threePoints.includes(lowerCasedWord[i])) subScore = 3
+          if (this.fourPoints.includes(lowerCasedWord[i])) subScore = 4
+          if (this.fivePoints.includes(lowerCasedWord[i])) subScore = 5
+          if (this.eightPoints.includes(lowerCasedWord[i])) subScore = 8
+          if (this.tenPoints.includes(lowerCasedWord[i])) subScore = 10
           finalScore += subScore * multiply
         }
       }

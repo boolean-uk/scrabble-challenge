6bd6db70a1886aedbc413826541369ce322ec1d1
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-03-16 10:33:06 +0100

    added robustness to multiplier

--- src/scrabble.js
+++ src/scrabble.js
@@ -6,14 +6,30 @@ class Scrabble {
   score () {
     let totalScore = 0
     let multiple = 1
+    let sumOfOpenSquareBracket = 0
+    let sumOfOpenCurlyBracket = 0
+    let sumOfCloseSquareBracket = 0
+    let sumOfCloseCurlyBracket = 0
     if (typeof this.word !== 'string') { return 0 }
     if (this.word === '\t' || this.word === '\n') { totalScore += 0 }
     for (let i = 0; i < this.word.length; i++) {
       const letter = letterValue[this.word[i].toLowerCase()] || 0
-      if (this.word[i] === '[') { multiple = 2 * multiple }
-      if (this.word[i] === '{') { multiple = 3 * multiple }
-      if (this.word[i] === ']') { multiple = multiple / 2 }
-      if (this.word[i] === '}') { multiple = multiple / 3 }
+      if (this.word[i] === '[') {
+        multiple = 2 * multiple
+        sumOfOpenSquareBracket += 1
+      }
+      if (this.word[i] === '{') {
+        multiple = 3 * multiple
+        sumOfOpenCurlyBracket += 1
+      }
+      if (this.word[i] === ']' && sumOfOpenSquareBracket > sumOfCloseSquareBracket) {
+        multiple = multiple / 2
+        sumOfCloseSquareBracket += 1
+      }
+      if (this.word[i] === '}' && sumOfOpenCurlyBracket > sumOfCloseCurlyBracket) {
+        multiple = multiple / 3
+        sumOfCloseCurlyBracket += 1
+      }
       totalScore += (letter * multiple)
     }
     return totalScore

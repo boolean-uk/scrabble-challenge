31633ce1ec662a5696815df7dd3f89b1aaed75c0
John Xander <johnblox5@gmail.com>
2022-05-25 14:14:17 +0100

    All extensions completed ✅

--- src/scrabble.js
+++ src/scrabble.js
@@ -12,22 +12,58 @@ class Scrabble {
   }
 
   score() {
+
     let totalScore = 0
 
-    if (this.input === null) {
-      totalScore = 0
-    } else {
-      this.input.split("").map(letter => {
-        const allCaps = letter.toUpperCase()
-        if (this.value1.includes(allCaps)) totalScore ++
-        if (this.value2.includes(allCaps)) totalScore += 2
-        if (this.value3.includes(allCaps)) totalScore += 3
-        if (this.value4.includes(allCaps)) totalScore += 4
-        if (this.value5.includes(allCaps)) totalScore += 5
-        if (this.value8.includes(allCaps)) totalScore += 8
-        if (this.value10.includes(allCaps)) totalScore += 10
-      })
-    }
+      if (this.input === null) {
+        totalScore = 0
+      } else {
+
+        let arrOfLetters = this.input.split("").map(letter => {
+          if (letter === "{") letter = "}*"
+          if (letter === "[") letter = "]%"
+          return letter
+        })
+    
+        if (arrOfLetters.includes("]")) arrOfLetters = arrOfLetters.join("").split("]")
+        if (arrOfLetters.includes("}")) arrOfLetters = arrOfLetters.join("").split("}")
+    
+        const isDouble = arrOfLetters.join("").includes("*")
+        const isTriple = arrOfLetters.join("").includes("%")
+    
+        let doubleArr, tripleArr
+    
+        if (isDouble) doubleArr = arrOfLetters.map(x => x.split(""))
+        if (isTriple) tripleArr = arrOfLetters.map(x => x.split(""))
+    
+        const getScore = arr => {
+          let score = 0
+          arr.map(letter => {
+            const allCaps = letter.toUpperCase()
+            if (this.value1.includes(allCaps)) score ++
+            if (this.value2.includes(allCaps)) score += 2
+            if (this.value3.includes(allCaps)) score += 3
+            if (this.value4.includes(allCaps)) score += 4
+            if (this.value5.includes(allCaps)) score += 5
+            if (this.value8.includes(allCaps)) score += 8
+            if (this.value10.includes(allCaps)) score += 10
+          })
+          return score
+        }
+    
+        if (isDouble) {
+          totalScore = doubleArr.map(x => {
+            return x.includes("*") ? getScore(x) * 2 : getScore(x)
+          }).reduce((a, b) => a + b)
+        } else if (isTriple) {
+          totalScore = tripleArr.map(x => {
+            return x.includes("%") ? getScore(x) * 3 : getScore(x)
+          }).reduce((a, b) => a + b)
+        } else {
+          totalScore = getScore(arrOfLetters)
+        }
+
+      }
     
     return totalScore
     

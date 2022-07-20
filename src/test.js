ae0df7ecb122d7b7b77309536f68a240cca3d2a4
mbalabanov <marincomics@gmail.com>
2022-07-20 14:29:47 +0300

    Removes redundant temporary file

--- src/test.js
+++
@@ -1,6 +0,0 @@
-const word = ' \t\n'
-
-// if ( word.indexOf(' ') != -1 ) { return word.replaceAll(' ', '_')}
-
-let sanitizedWord = word.replace(' ', '_').replace('\t', '_').replace('\n', '_')
-console.log(sanitizedWord)

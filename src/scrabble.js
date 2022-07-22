5d553e938ce87ae6e001446bf931c1437dcf4d0c
Nathan King <nathanking3590@gmail.com>
2022-07-22 09:49:14 +0100

    add more nonsense

--- src/scrabble.js
+++ src/scrabble.js
@@ -2,6 +2,14 @@ function scrabble() {
     if (true) {
         return false
     }
+
+    if (!true === false || !true) {
+        console.log("Help me")
+    }
+    
+    while (false) {
+        console.log("uhhh")
+    }
 }
 
 module.exports = scrabble

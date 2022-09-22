f5ff4329cb94064f8f4ce122515f0ac56bfdb56b
Joel Watkins <eck087@yahoo.co.uk>
2022-09-22 14:47:31 +0100

    cleaned up a bit, few extra comments

--- src/scrabble.js
+++ src/scrabble.js
@@ -25,10 +25,6 @@ const points = {
   x: 8,
   q: 10,
   z: 10
-  // '{': 0,
-  // '}': 0,
-  // '[': 0,
-  // ']': 0
 }
 
 function scrabble(word) {
@@ -38,11 +34,6 @@ function scrabble(word) {
   // force the word to lower case and then split into an array for easier looping
   const wordArray = word.toLowerCase().split('')
   let score = 0
-
-  // loop through the array and, accessing the object, we can add the correct points to the score
-  // for (let i = 0; i < wordArray.length; i++) {
-  //   score += points[wordArray[i]]
-  // }
 
   // forEach version, nicer looking and easier to understand
   wordArray.forEach((letter) => {
@@ -57,6 +48,8 @@ function scrabble(word) {
   return score
 }
 
+// takes in the full array and score. We loop through each letter in the array, and if we detect a certain bracket with a closing one 2
+// indexes later, we adjust the score and return. If these brackets aren't detected, we return the original score and exit the function
 function letterBonusChecker(wordArray, score) {
   for (let j = 0; j < wordArray.length; j++) {
     if (wordArray[j] === '{' && wordArray[j + 2] === '}') {
@@ -84,12 +77,12 @@ function wordBonusChecker(wordArray, score) {
   }
 }
 
-console.log(scrabble('do[g]')) // 2 + 1 + 6 = should be 9
-console.log(scrabble('{dog}')) // (2 + 1 + 2) * 2 = should be 10
-console.log(scrabble('[dog]')) // should be 15
-console.log(scrabble('quirky')) // should be 22
-console.log(scrabble('{O}XYPHENBUTAZONE')) // should be 42
-console.log(scrabble('OXYPHENBUTAZ{O}NE')) // should also be 42
-console.log(scrabble('hel!o'))
+console.log('do[g]', scrabble('do[g]')) // 2 + 1 + 6 = should be 9
+console.log('{dog}', scrabble('{dog}')) // (2 + 1 + 2) * 2 = should be 10
+console.log('[dog]', scrabble('[dog]')) // should be 15
+console.log('cabbage', scrabble('cabbage')) // should be 22
+console.log('{O}XYPHENBUTAZONE', scrabble('{O}XYPHENBUTAZONE')) // should be 42
+console.log('OXYPHENBUTAZ{O}NE', scrabble('OXYPHENBUTAZ{O}NE')) // should also be 42
+console.log('hel!o', scrabble('hel!o'))
 
 module.exports = scrabble

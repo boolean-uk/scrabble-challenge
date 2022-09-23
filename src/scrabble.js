27e4f031fcb8adec57ae1b4dded2ab8e126427d3
wagnerAJS221 <wagneralex221@gmai.com>
2022-09-23 11:19:53 +0100

    complete scrabble challenge and updated my pseudo code

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,44 @@
-function scrabble() {
+function scrabble(scrabbleWord) {
+  if (!scrabbleWord) return 0
+  const libary = {
+    a: 1,
+    e: 1,
+    i: 1,
+    o: 1,
+    u: 1,
+    l: 1,
+    n: 1,
+    r: 1,
+    s: 1,
+    t: 1,
+    d: 2,
+    g: 2,
+    b: 3,
+    c: 3,
+    m: 3,
+    p: 3,
+    f: 4,
+    h: 4,
+    v: 4,
+    w: 4,
+    y: 4,
+    k: 5,
+    j: 8,
+    x: 8,
+    q: 10,
+    z: 10
+  }
+  let sum = 0
+  scrabbleWord = scrabbleWord.toLowerCase()
 
+  for (let i = 0; i < scrabbleWord.length; i++) {
+    sum += libary[scrabbleWord[i]] || 0
+    // double or triple word to come here ( look at if statements )
+  }
+
+  return sum
 }
+
+console.log(scrabble(null))
 
 module.exports = scrabble

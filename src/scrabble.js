809a8130e33fae65bba11cd2118d25764f53134a
Dave <Daverevell491@gmail.com>
2022-09-23 16:57:55 +0100

    Ext. step 1 and 2 implemented

--- src/scrabble.js
+++ src/scrabble.js
@@ -2,9 +2,17 @@ const values = require('./letter-values.js') // fetch the values table
 
 function scrabble(word) {
   let points = 0
-  if (word != null) {
-    if (word.includes('{', '}')) {
-      // placeholder
+  if (word != null) { // safeguarding against no value
+    if (word.includes('{', '}')) { // if word contains brackets
+      const firstBracket = word.indexOf('{') // creates variable that's the index of the first bracket location
+      const secondBracket = word.indexOf('}') // same as above but for the second bracket
+      console.log(firstBracket)
+      if (firstBracket !== word[0] && secondBracket !== word[word.length - 1]) {
+        // checking if the brackets include the whole word, if so ignore this and continue
+        const toBeDoubled = word.substring(firstBracket + 1, secondBracket) // create a variable that includes the letters within the brackets
+        console.log(toBeDoubled)
+        console.log(word)
+      }
     }
     const character = word.split('') // split the given word into individual characters in an array
     character.forEach((letter) => {
@@ -14,10 +22,14 @@ function scrabble(word) {
       points *= 2
       console.log('Extension 2')
     }
+    if (word[0].includes('[') && word[word.length - 1].includes(']')) {
+      points *= 3
+      console.log('Extension 2')
+    }
   }
   return points
 }
 
-console.log(scrabble('c{a}bbage'))
+console.log(scrabble('c{abbag}e'))
 
 module.exports = scrabble

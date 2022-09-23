b7bc1aa8521f7d1562a10fb5750d54cc98347908
Dave <Daverevell491@gmail.com>
2022-09-23 18:11:35 +0100

    Ext. 1 completed

--- src/scrabble.js
+++ src/scrabble.js
@@ -4,21 +4,27 @@ function scrabble(word) {
   let points = 0
   if (word != null) {
     // safeguarding against no value
+    const character = word.split('')
+    // split the given word into individual characters in an array
     if (word.includes('{', '}')) {
       // if word contains brackets
       const firstBracket = word.indexOf('{') // creates variable that's the index of the first bracket location
       const secondBracket = word.indexOf('}') // same as above but for the second bracket
-      console.log(firstBracket)
       if (firstBracket !== word[0] && secondBracket !== word[word.length - 1]) {
         // checking if the brackets include the whole word, if so ignore this and continue
-        const toBeDoubled = word.substring(firstBracket + 1, secondBracket) // create a variable that includes the letters within the brackets
+        // const toBeDoubled = word.substring(firstBracket + 1, secondBracket) // create a variable that includes the letters within the brackets
+        const toBeDoubled = character.splice(firstBracket, secondBracket) // create a variable that is the letters within the bracket
         console.log(toBeDoubled)
-        console.log(word)
+        console.log(character)
+        toBeDoubled.forEach((doubledLetter) => {
+          points += values[doubledLetter.toLowerCase()] * 2 || 0
+          // change every letter to lowercase, match the letter with the letter and value in the values array and add to the points variable, or if it's not in the array, return 0 points
+        })
       }
     }
-    const character = word.split('') // split the given word into individual characters in an array
     character.forEach((letter) => {
-      points += values[letter.toLowerCase()] || 0 // change every letter to lowercase, match the letter with the letter and value in the values array and add to the points variable, or if it's not in the array, return 0 points
+      points += values[letter.toLowerCase()] || 0
+      // change every letter to lowercase, match the letter with the letter and value in the values array and add to the points variable, or if it's not in the array, return 0 points
     })
     if (word[0].includes('{') && word[word.length - 1].includes('}')) {
       points *= 2
@@ -32,6 +38,6 @@ function scrabble(word) {
   return points
 }
 
-console.log(scrabble('c{abbag}e'))
+console.log(scrabble('b{a}ggage'))
 
 module.exports = scrabble

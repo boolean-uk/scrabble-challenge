df2a1df6c16c955419c7058524504817daa06c4b
OlhaDorofiychuk <olha.d@yahoo.com>
2022-09-26 15:35:11 +0100

    extra function with amendments

--- src/scrabble.js
+++ src/scrabble.js
@@ -26,35 +26,61 @@ const scoreTable = {
   Q: 10,
   Z: 10
 }
+
+function scoreForLetter(letter) {
+  return scoreTable[letter.toUpperCase()] || 0
+}
+
 function scrabble(word) {
   let totalScore = 0
-  //   I have realy struggled without lines 32 and 32. my code was failing to proceed with line 35 without this guard.
+  //   I have realy struggled without lines 37. my code was failing to proceed with line 40 without this guard.
   if (!word) {
     return 0
   }
-  const upperCasedString = word.toUpperCase().split('')
+  const upperCasedString = word.split('')
   upperCasedString.forEach((letter) => {
-    totalScore += scoreTable[letter] || 0
+    totalScore += scoreForLetter(letter)
   })
   return totalScore
 }
 console.log(scrabble('Dorofi +ychuk'))
 
 function doubleWord(word) {
-  if ((word[0] === '{') & (word[word.length - 1] === '}')) {
+  if (word[0] === '{' && word[word.length - 1] === '}') {
     return scrabble(word) * 2
   } else {
     return scrabble(word)
   }
 }
 
+console.log(doubleWord('{dorofiychuk}'))
+
 function tripleWord(word) {
-  if ((word[0] === '[') & (word[word.length - 1] === ']')) {
+  if (word[0] === '[' && word[word.length - 1] === ']') {
     return scrabble(word) * 3
   } else {
     return scrabble(word)
   }
 }
 console.log(tripleWord('[dorofi-ychuk ]'))
-console.log(doubleWord('{dorofiychuk}'))
+
+function twoSameLettersInLine(word) {
+  const initScore = scrabble(word)
+  let bonusPoints = 0
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i]
+    if (word[i] === word[i - 1]) {
+      bonusPoints += scoreForLetter(letter) * 3
+      //   return scrabble(word) + bonusPoints
+    }
+  }
+  console.log('bonus', bonusPoints)
+  console.log('score', initScore)
+
+  return initScore + bonusPoints
+}
+
+console.log(twoSameLettersInLine('Doorofiychuk'))
+
 module.exports = scrabble

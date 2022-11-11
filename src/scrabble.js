e2dcb9c3db2457ad1d7ae941b1cb04896757d164
dearshrewdwit <dearshrewdwit@gmail.com>
2022-11-11 11:06:18 +0000

    Add solution

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,66 @@
-function scrabble() {
+const points = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
+}
 
+const multiplierTokens = {
+  '{': '}',
+  '[': ']'
+}
+
+const multiplierValues = {
+  '{': 2,
+  '[': 3
+}
+
+const scrabble = (str) => {
+  str = (str || '').toUpperCase()
+
+  if (str.length === 0) return 0
+  if (!!str.match(/[^\w{}\[\]]/g)) return 0
+
+  const chars = str.split('')
+  const multipliers = []
+  let wordMultiplier = 1
+  let letterMultiplier = 1
+  let score = 0
+
+  for (let i=0; i<chars.length; i++) {
+    const char = chars[i]
+
+    if (Object.keys(multiplierTokens).includes(char) && chars[i+2] !== multiplierTokens[char]) {
+      wordMultiplier *= multiplierValues[char]
+      multipliers.push(char)
+      continue
+    } else if (Object.keys(multiplierTokens).includes(char) && chars[i+2] === multiplierTokens[char]) {
+      letterMultiplier *= multiplierValues[char]
+      multipliers.push(char)
+      continue
+    }
+
+    if (Object.values(multiplierTokens).includes(char)) {
+      multipliers.push(char)
+      if (chars[i-2] === Object.keys(multiplierTokens).find(open => multiplierTokens[open] === char)) {
+        letterMultiplier = 1
+      }
+      continue
+    }
+
+    score += findPtsFor(char) * letterMultiplier
+  }
+
+  if (multipliers.length % 2 !== 0) return 0
+
+  return score * wordMultiplier
+}
+
+const findPtsFor = (char) => {
+  return Number(Object.keys(points).find(key => points[key].includes(char)))
 }
 
 module.exports = scrabble

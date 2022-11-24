915f6cfa29a7dc03499a4a2749f43e3f1da0cc2f
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2022-11-24 16:49:27 +0000

    Extension - Passed 1 spec: d{o}g equals 6.

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,8 @@
 function scrabble(string) {
-  let score = 0
+  // let score = 0
 
   if (!string) {
-    return score
+    return 0
   }
 
   const word = string.toUpperCase()
@@ -15,43 +15,77 @@ function scrabble(string) {
   const scoreOfEight = ['J', 'X']
   const scoreOfTen = ['Q', 'Z']
 
-  for (let i = 0; i < word.length; i++) {
-    const counter = i
-    for (let j = 0; j < scoreOfOne.length; j++) {
-      if (word[counter] === scoreOfOne[j]) {
-        score += 1
+  function calculateScore(word) {
+    let score = 0
+    for (let i = 0; i < word.length; i++) {
+      const counter = i
+
+      for (let j = 0; j < scoreOfOne.length; j++) {
+        if (word[counter] === scoreOfOne[j]) {
+          score += 1
+        }
       }
-    }
-    for (let j = 0; j < scoreOfTwo.length; j++) {
-      if (word[counter] === scoreOfTwo[j]) {
-        score += 2
+      for (let j = 0; j < scoreOfTwo.length; j++) {
+        if (word[counter] === scoreOfTwo[j]) {
+          score += 2
+        }
       }
-    }
-    for (let j = 0; j < scoreOfThree.length; j++) {
-      if (word[counter] === scoreOfThree[j]) {
-        score += 3
+      for (let j = 0; j < scoreOfThree.length; j++) {
+        if (word[counter] === scoreOfThree[j]) {
+          score += 3
+          // console.log(`${counter} and score is: ${score}`)
+        }
       }
-    }
-    for (let j = 0; j < scoreOfFour.length; j++) {
-      if (word[counter] === scoreOfFour[j]) {
-        score += 4
+      for (let j = 0; j < scoreOfFour.length; j++) {
+        if (word[counter] === scoreOfFour[j]) {
+          score += 4
+        }
       }
-    }
-    if (word[counter] === scoreOfFive) {
-      score += 5
-    }
-    for (let j = 0; j < scoreOfEight.length; j++) {
-      if (word[counter] === scoreOfEight[j]) {
-        score += 8
+      if (word[counter] === scoreOfFive) {
+        score += 5
       }
-    }
-    for (let j = 0; j < scoreOfTen.length; j++) {
-      if (word[counter] === scoreOfTen[j]) {
-        score += 10
+      for (let j = 0; j < scoreOfEight.length; j++) {
+        if (word[counter] === scoreOfEight[j]) {
+          score += 8
+        }
       }
+      for (let j = 0; j < scoreOfTen.length; j++) {
+        if (word[counter] === scoreOfTen[j]) {
+          score += 10
+        }
+      }
+      // console.log(`${counter} and score is: ${score}`)
     }
+    return score
   }
-  return score
+
+  function doubleScore() {
+    for (let i = 0; i < word.length; i++) {
+      const counter = i
+      let charsToBeDoubled = '.'
+      if (word[counter] === '{') {
+        // while (word[counter] !== '}') {
+        for (let i = word.indexOf(word[counter]); i < word.length; i++) {
+          const counter = i
+          if (word[counter] === '}') {
+            break
+          }
+          charsToBeDoubled += word[counter]
+        }
+
+        // console.log('INSIDE DOUBLE SCORE ---> ', charsToBeDoubled)
+        return calculateScore(charsToBeDoubled)
+        // }
+      }
+    }
+    // score = 0
+    return 0
+  }
+
+  const finalScore = calculateScore(word) + doubleScore()
+  return finalScore
 }
+
+// console.log(scrabble('d{o}g'))
 
 module.exports = scrabble

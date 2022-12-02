104dae665439155b96cfc0a497693d01c5cfe631
Nozie <nosizodube6@gmail.com>
2022-12-02 17:15:10 +0000

    scrabble completed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,51 +1,47 @@
 const scoreMap = {
-    A: 1,
-    E: 1,
-    I: 1,
-    O: 1,
-    U: 1,
-    L: 1,
-    N: 1,
-    R: 1,
-    S: 1,
-    T: 1,
-    D: 2,
-    G: 2,
-    B: 3,
-    C: 3,
-    M: 3,
-    P: 3,
-    F: 4,
-    H: 4,
-    V: 4,
-    W: 4,
-    Y: 4,
-    K: 5,
-    J: 8,
-    X: 8,
-    Q: 10,
-    Z: 10,
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10
 }
 
 function scrabble(anyword) {
-if (anyword === null) {
-    return 0;
-}
+  if (anyword === null) {
+    return 0
+  }
 
+  let score = 0
 
-let score = 0
-
-const trimmedWord = anyword.trim().toUpperCase()
-for (let i = 0; i < trimmedWord.length; i++) {
+  const trimmedWord = anyword.trim().toUpperCase()
+  for (let i = 0; i < trimmedWord.length; i++) {
     const letter = trimmedWord[i]
 
-
     score += scoreMap[letter]
+  }
+
+  return score
 }
-
-return score
-}
-
-
 
 module.exports = scrabble

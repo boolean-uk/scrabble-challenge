81dd900449c93329a47ecde775ea89beb1ac93c8
Todd Griffin <todd.griffin61@outlook.com>
2023-03-15 13:28:38 +0000

    linted

--- src/scrabble.js
+++ src/scrabble.js
@@ -27,27 +27,72 @@ const charScores = {
   z: 10
 }
 
-function scrabble(string) {
-  if (typeof string !== 'string' || string === '') {
-    console.log('Please enter a valid word')
-    return 0
-  }
+const findDoubleChars = (string) => {
+  const matched = string.match(/\{(.*?)\}/)
+  return matched
+}
 
+console.log(findDoubleChars('aaa[bb]'))
+
+const findTripleChars = (string) => {
+  const matched = string.match(/\[(.*?)\]/)
+  return matched
+}
+
+const getScoreOfPlainString = (string, multiplier) => {
   let totalScore = 0
   string = string.toLowerCase()
   for (const i in string) {
     if (string[i] === ' ' || string[i] === '\t' || string[i] === '\n') {
-      console.log('oops')
+      // console.log('oops')
     } else {
-      console.log(string[i])
-      console.log(charScores)
-      console.log(charScores[string[i]])
+      // console.log(string[i])
+      // console.log(charScores)
+      // console.log(charScores[string[i]])
       totalScore += charScores[string[i]]
     }
   }
+  return totalScore * multiplier
+}
+
+const stringIsValid = (string) => {
+  if (typeof string !== 'string' || string === '') {
+    // console.log('Please enter a valid word')
+    return false
+  } else if (
+    (findTripleChars(string) === null && string.search(/\[|\]/) !== -1) ||
+    (findDoubleChars(string) === null && string.search(/\{|\}/) !== -1)
+  ) {
+    return false
+  } else if (string.search(/\|/) !== -1) {
+    return false
+  }
+  return true
+}
+
+function scrabble(string) {
+  if (!stringIsValid(string)) {
+    return 0
+  }
+
+  let totalScore = 0
+  while (findDoubleChars(string) !== null) {
+    console.log(string)
+    console.log(findDoubleChars(string)[0])
+    totalScore += getScoreOfPlainString(findDoubleChars(string)[1], 2)
+    // remove matched string from input string
+    string = string.replace(findDoubleChars(string)[0], '')
+  }
+
+  while (findTripleChars(string) !== null) {
+    totalScore += getScoreOfPlainString(findTripleChars(string)[1], 3)
+    // remove matched string from input string
+    string = string.replace(findTripleChars(string)[0], '')
+  }
+  totalScore += getScoreOfPlainString(string, 1)
   return totalScore
 }
 
-console.log(scrabble(' '))
+console.log(scrabble('[dog]'))
 
 module.exports = scrabble

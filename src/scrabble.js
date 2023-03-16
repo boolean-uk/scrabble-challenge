38101047b8713341cb279c263404e0953e4cd0e8
Ian Grantham <dev.iangrantham@gmail.com>
2023-03-16 19:34:07 +0000

    trying forEach approach

--- src/scrabble.js
+++ src/scrabble.js
@@ -8,6 +8,16 @@ const pointValuesForLetters = [
   { letters: ['Q', 'Z'], points: 10 }
 ]
 
+// // function takes any word and returns scrabble score
+function scrabble(word) {
+  const score = 0
+  const givenWord = upperString(word)
+  console.log('what is givenWord?', givenWord)
+  const givenLetter = givenWord.split('')
+  searchMainArray(pointValuesForLetters, givenLetter)
+  return score
+}
+
 // function turns all given letters to capitals
 const upperString = (aString) => {
   if (typeof aString === 'string') {
@@ -17,56 +27,60 @@ const upperString = (aString) => {
   }
 }
 
-// look through pointsValuesForLetters array (loop)
-//    first object - extract inner Array [letters]
-//        includes letter we're looking for?
-//           if true - output score and stop looking
-//            if false - look at second object
-//                  etc...
+// // look through pointsValuesForLetters array (loop)
+// const searchMainArray = (arrayToSearch, givenLetter) => {
+//   const arraySearching = arrayToSearch
+//   for (let i = 0; i <= arraySearching.length - 1; i++) {
+//     // each time - extract object at index i
+//     const innerObject = Object.values(pointValuesForLetters[i])
+//     // call function that compares, pass innerObject through
+//     compareGivenLetter(innerObject, givenLetter)
+//   }
+// }
 
-// look through pointsValuesForLetters array (loop)
 const searchMainArray = (arrayToSearch, givenLetter) => {
-  const arraySearching = arrayToSearch
-  for (let i = 0; i <= arraySearching.length - 1; i++) {
-    // each time - extract object at index i
-    const innerObject = Object.values(pointValuesForLetters[i])
-    // call function that compares, pass innerObject through
-    compareGivenLetter(innerObject, givenLetter)
-  }
+  const arrayToSearch2 = arrayToSearch
+  const givenLetter2 = givenLetter
+arrayToSearch.forEach(element => {
+  compareGivenLetter(arrayToSearch2, givenLetter2)
+});
 }
 
 // includes letter we're looking for?
 const compareGivenLetter = (innerObject, givenLetter) => {
   const lettersArray = innerObject
-  let score = 0
-  console.log('score before if - ', score)
+  // const letArrString = lettersArray.toString()
+  const newGivenLetter = givenLetter.toString()
+  let currentScore = 0
+  console.log('currentScore before if - ', currentScore)
   console.log('letArr[1] = ', lettersArray[1])
   console.log('letArr[0] = ', lettersArray[0])
-  console.log('givLetter = ', givenLetter)
+  console.log('givLetter = ', newGivenLetter)
+  // console.log('lettArrString =', letArrString)
   // does given letter appear in this array?
-  if (lettersArray[0].includes(givenLetter)) {
-    console.log('found!')
-    score += lettersArray[1]
-    console.log('score after if - ', score)
+  if (lettersArray[0].includes(newGivenLetter)) {
+    console.log('-*- FOUND! -*- ')
+    currentScore += lettersArray[1]
+    console.log('currentScore after if - ', currentScore)
   } else {
-    score = 0
+    console.log(' - Not Found -')
+    currentScore = 0
   }
-  return score
+  console.log('--- returning currentScore ---')
+  return currentScore
 }
 
-// // function takes any word and returns scrabble score
-function scrabble(word) {
-  // const score = 0
-  const givenWord = upperString(word)
-  console.log('what is givenWord?', givenWord)
-  const givenLetter = givenWord.split('')
-  searchMainArray(pointValuesForLetters, givenLetter)
-}
-
-console.log(scrabble('z'))
+console.log(scrabble('a'))
 
 module.exports = scrabble
 
 // --- refactor plan ---
 // a - combine point and letter arrays into single array
 // b - make function for iterating through checks to find letter
+
+// look through pointsValuesForLetters array (loop)
+//    first object - extract inner Array [letters]
+//        includes letter we're looking for?
+//           if true - output score and stop looking
+//            if false - look at second object
+//                  etc...

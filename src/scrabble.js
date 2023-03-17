d14d71bc75cb8a1a7e68250e6d2c4e6218db20a1
Ian Grantham <dev.iangrantham@gmail.com>
2023-03-17 13:56:20 +0000

    refactored functions working for core

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,4 +1,4 @@
-const pointValuesForLetters = [
+const referenceArray = [
   [1, 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
   [2, 'D', 'G'],
   [3, 'B', 'C', 'M', 'P'],
@@ -7,95 +7,67 @@ const pointValuesForLetters = [
   [8, 'J', 'X'],
   [10, 'Q', 'Z']
 ]
+
+// --- SCORING ---
+// keep track of the score
 let score = 0
-
-// // function takes any word and returns scrabble score
-function scrabble(word) {
-  // word to upper case
-  const givenWord = caseSensitive(word)
-  // UC word split to array of letters
-  const givenLetters = splitLetters(givenWord)
-  // for each of the given letters, loop ref. array
-  letterLookingFor(givenLetters)
-}
-
-// function turns all given letters to capitals
-const caseSensitive = (aString) => {
-  if (typeof aString === 'string') {
-    return aString.toUpperCase()
-  } else {
-    return ''
-  }
-}
-
-// split UC word into Array of Letters
-const splitLetters = (givenWord) => {
-  const givenLetters = givenWord.split('')
-  console.log('givenLetters =', givenLetters)
-  return givenLetters
-}
-
-// for each element of givenLetters, loop through reference array
-const letterLookingFor = (letterToCompare) => {
-  const questionedLetter5 = letterToCompare
-  letterToCompare.forEach(() => {
-    searchMainArray(pointValuesForLetters, questionedLetter5)
-  })
-}
-
-// look through pointsValuesForLetters array (loop)
-const searchMainArray = (mainArray, questionedLetter4) => {
-  const outerArray = mainArray
-  const questionedLetter3 = questionedLetter4
-  for (let i = 0; i <= outerArray.length - 1; i++) {
-    // each time - call function that searches that element (inner array)
-    searchInnerArray(outerArray[i], questionedLetter3)
-  }
-}
-
-// loop through elements of inner array, start idx 1
-const searchInnerArray = (subArray, questionedLetter) => {
-  const innerArray = subArray
-  const questionedLetter1 = questionedLetter
-  console.log(innerArray)
-  for (let j = 1; j <= innerArray.length - 1; j++) {
-    compareInnerArray(innerArray, questionedLetter1)
-  }
-}
-
-// compare given letter with inner array
-const compareInnerArray = (innerArrayElement, questionedLetter2) => {
-  const searchingList = innerArrayElement
-  console.log('searchingList =', searchingList)
-  const lookingFor = questionedLetter2
-  console.log('lookingFor =', lookingFor)
-  // compare the letter we're searching for with the reference array element we are looking at
-  if (searchingList.includes(lookingFor)) {
-    // score some points - point value from array letter found in
-    console.log('FOUND!')
-    pointScored(innerArrayElement[0])
-  } else {
-    // score 0 points
-    noPoints()
-  }
-  return score
-}
-
 // score some points
 const pointScored = (pointValue) => {
   score += pointValue
   return score
 }
 // score 0 points
-const noPoints = () => {
+const zeroPoints = () => {
   score = 0
   return score
 }
 
-console.log(scrabble('z'))
+// --- INPUT A WORD ---
+// // function takes any word and returns scrabble score
+function scrabble(word) {
+  // word to upper case
+  const givenWord = caseSensitive(word)
+  // UC word split to array of letters
+  const givenLettersArray = splitLetters(givenWord)
+  // for each of the given letters, loop ref. array
+  letterLookingFor(givenLettersArray)
+  console.log('---')
+  return score
+}
+
+// function checks valid string input, turns all given letters to capitals
+const caseSensitive = (aString) => {
+  if (typeof aString === 'string') {
+    return aString.toUpperCase()
+  } else {
+    return zeroPoints()
+  }
+}
+
+// split UC word into Array of Letters
+const splitLetters = (givenWord) => {
+  const givenLettersArray = givenWord.split('')
+  console.log('givenLetters =', givenLettersArray)
+  return givenLettersArray
+}
+
+// loop through array of given letters
+const letterLookingFor = (givenArrayOfLetters) => {
+  const letterToCompare = givenArrayOfLetters
+  for (let i = 0; i < letterToCompare.length; i++) {
+    // for each given letter, loop through the reference array
+    for (let j = 0; j <= referenceArray.length - 1; j++) {
+      if (referenceArray[j].includes(letterToCompare[i])) {
+        // return the associated number of points for that letter
+        console.log(
+          `${letterToCompare[i]} scored ${referenceArray[j][0]} points!`
+        )
+        pointScored(referenceArray[j][0])
+      }
+    }
+  }
+}
+
+console.log(scrabble('example'))
 
 module.exports = scrabble
-
-// --- refactor plan ---
-// a - combine point and letter arrays into single array
-// b - make function for iterating through checks to find letter

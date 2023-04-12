9557245db86b13a5ac89a35201a640655eba2f65
Aidan van Geest <aidanvangeest@gmail.com>
2023-04-12 18:36:03 +0200

    Standard tests

--- pseudocode.md
+++ pseudocode.md
@@ -3,11 +3,21 @@
 #### Add the steps you need to take to implement the requirements for Scrabble Challenge
 
 ```
-1.
+Standard:
 
-2.
+1. Create a variable that stores a string to be assessed for scrabble points.
 
-3.
+2. Create a for loop that runs through the characters of the string (as an array of characters).
 
-...etc
+3. Use conditional statements to evaluate each character for points using the defined points table.
+
+Extended:
+
+1. Use a regex to search through the string and return a substring of any characters encapsulated in {} or []
+
+2. Double or triple the points tally of that letter using the scrable function created above
+
+3. Use a regex to search through the string and return a substring of any words encapsulated in {} or []
+
+4. Double or triple the points tally of that entire word using the scrable function created above
 ```
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,58 @@
-function scrabble() {
-  // write code here
+function scrabble(word) {
+  let points = 0
+
+  if (word === null || word === undefined) {
+    points = 0
+  } else {
+    word = word.toUpperCase()
+    // console.log(word)
+
+    for (let i = 0; i < word.length; i++) {
+      if (
+        word[i] === 'A' ||
+        word[i] === 'E' ||
+        word[i] === 'I' ||
+        word[i] === 'O' ||
+        word[i] === 'U' ||
+        word[i] === 'L' ||
+        word[i] === 'N' ||
+        word[i] === 'R' ||
+        word[i] === 'S' ||
+        word[i] === 'T'
+      ) {
+        points += 1
+      } else if (word[i] === 'D' || word[i] === 'G') {
+        points += 2
+      } else if (
+        word[i] === 'B' ||
+        word[i] === 'C' ||
+        word[i] === 'M' ||
+        word[i] === 'P'
+      ) {
+        points += 3
+      } else if (
+        word[i] === 'F' ||
+        word[i] === 'H' ||
+        word[i] === 'V' ||
+        word[i] === 'W' ||
+        word[i] === 'Y'
+      ) {
+        points += 4
+      } else if (word[i] === 'K') {
+        points += 5
+      } else if (word[i] === 'J' || word[i] === 'X') {
+        points += 8
+      } else if (word[i] === 'Q' || word[i] === 'Z') {
+        points += 10
+      } else {
+        points += 0
+      }
+    }
+  }
+
+  return points
 }
+
+console.log(scrabble('OXYPHENBUTAZONE'))
 
 module.exports = scrabble

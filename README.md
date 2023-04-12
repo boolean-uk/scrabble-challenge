d867d35994c964a2d93f34e96808c42dae6da5d2
Valentina <valentinagalyam@gmail.com>
2023-04-12 15:54:15 +0200

    scabble is working

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ You'll need these:
 | D, G                          |     2  |
 | B, C, M, P                    |     3  |
 | F, H, V, W, Y                 |     4  |
-| K                             |     5  |
+| K        n                     |     5  |
 | J, X                          |     8  |
 | Q, Z                          |     10 |
 
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,46 @@
-function scrabble() {
-  // write code here
+// const word = 'street'
+function scrabble(word) {
+  const values = {
+    A: 1,
+    E: 1,
+    I: 1,
+    O: 1,
+    U: 1,
+    L: 1,
+    N: 1,
+    R: 1,
+    S: 1,
+    T: 1,
+    D: 2,
+    G: 2,
+    B: 3,
+    C: 3,
+    M: 3,
+    P: 3,
+    F: 4,
+    H: 4,
+    V: 4,
+    W: 4,
+    Y: 4,
+    K: 5,
+    J: 8,
+    X: 8,
+    Q: 10,
+    Z: 10,
+    '': 0
+  }
+  let score = 0
+  if (word !== null) {
+    word = word.toUpperCase()
+
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] in values) {
+        score += values[word[i]]
+      } else score = 0
+    }
+  }
+  return score
 }
+// console.log(scrabble())
 
 module.exports = scrabble

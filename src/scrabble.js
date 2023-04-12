f4ccba90b1904335cce07836e9d0f77753a96376
Rose <rosevandergiessen@proton.me>
2023-04-12 12:02:28 +0200

    implemented scrabble function

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,40 @@
-function scrabble() {
-  // write code here
+function scrabble(word) {
+
+  let onePoint = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+  let twoPoint = ['D', 'G']
+  let threePoint = ['B', 'C', 'M', 'P']
+  let fourPoint = ['F', 'H', 'V', 'W', 'Y']
+  let fivePoint = ['K']
+  let eightPoint = ['J', 'X']
+  let tenPoint = ['Q', 'Z']
+
+  let score = 0
+
+  if (word === '' || word === undefined || word === null) {
+    score = 0
+    return score
+  } else {
+    let letters = word.split('')
+    for (let i = 0; i < letters.length; i++) {
+      let capitalLetter = letters[i].toUpperCase()
+      if (onePoint.includes(capitalLetter)) {
+        score += 1
+      } else if (twoPoint.includes(capitalLetter)) {
+        score += 2
+      } else if (threePoint.includes(capitalLetter)) {
+        score += 3
+      } else if (fourPoint.includes(capitalLetter)) {
+        score += 4
+      } else if (fivePoint.includes(capitalLetter)) {
+        score += 5
+      } else if (eightPoint.includes(capitalLetter)) {
+        score += 8
+      } else if (tenPoint.includes(capitalLetter)) {
+        score += 10
+      }
+    }
+    return score
+  }
 }
 
 module.exports = scrabble

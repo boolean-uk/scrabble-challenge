cb6fa16d5623abddd3ebcc64d9b44c147d12aba6
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-12 19:18:24 +0200

    finished the Acceptance Criteria

--- pseudocode.md
+++ pseudocode.md
@@ -3,11 +3,34 @@
 #### Add the steps you need to take to implement the requirements for Scrabble Challenge
 
 ```
-1.
+make a program that calculates the word value of words for scrabble
 
-2.
+create a variable to store the word value in
 
-3.
+define the letter values
+A, E, I, O, U, L, N, R, S, T = 1
+D, G = 2
+B, C, M, P = 3
+F, H, V, W, Y = 4
+K = 5
+J, X = 8
+Q, Z = 10
+other than that appoint 0
+
+
+assign the dedicated values to lowercase letters
+
+
+make the given word lowercase
+
+
+make every letter of the given word seperate, not needed?
+
+
+loop every letter of the given word to assign the dedicated value to it
+
+
+calculate the value of the word by adding the values of the letters togetter
 
 ...etc
 ```
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,43 @@
-function scrabble() {
-  // write code here
+const letterValue1 = ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
+const letterValue2 = ['d', 'g']
+const letterValue3 = ['b', 'c', 'm', 'p']
+const letterValue4 = ['f', 'h', 'v', 'w', 'y']
+const letterValue5 = ['k']
+const letterValue8 = ['j', 'x']
+const letterValue10 = ['q', 'z']
+
+function scrabble(word) {
+  let wordValue = 0
+  if (word !== null) {
+    word = word.toLowerCase().split('')
+
+    for (let i = 0; i < word.length; i++) {
+      if (letterValue1.includes(word[i])) {
+        wordValue += 1
+      }
+      if (letterValue2.includes(word[i])) {
+        wordValue += 2
+      }
+      if (letterValue3.includes(word[i])) {
+        wordValue += 3
+      }
+      if (letterValue4.includes(word[i])) {
+        wordValue += 4
+      }
+      if (letterValue5.includes(word[i])) {
+        wordValue += 5
+      }
+      if (letterValue8.includes(word[i])) {
+        wordValue += 8
+      }
+      if (letterValue10.includes(word[i])) {
+        wordValue += 10
+      }
+    }
+  }
+  return wordValue
 }
+
+console.log(scrabble(null))
 
 module.exports = scrabble

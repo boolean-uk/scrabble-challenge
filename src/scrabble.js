71afee5c84b87734cf3b9abdbe077304926f46fb
John Poikilidis <akiskawi@gmail.com>
2023-04-12 17:56:42 +0300

    Clean Code/Readable

--- src/scrabble.js
+++ src/scrabble.js
@@ -30,41 +30,72 @@ const letterValues = {
   '[': 3,
   ']': 3
 }
-function scrabble(word) {
-  let invalid = 0
-  let mupliplyer = 1
-  let wordValue = 0
+function isValidLetter(letter) {
+  return !Object.keys(letterValues).includes(letter)
+}
+function isValidWord(word) {
   if (word === undefined || word === null) {
-    // console.log('exit here, code: null||undefined')
-    return wordValue
+    return false
   }
-  const letters = word.trim().toUpperCase().split('')
-  letters.forEach((letter) => {
-    if (!Object.keys(letterValues).includes(letter)) {
-      invalid = 1
-      return
+  return true
+}
+function isOpenBraket(braket, multiplier) {
+  if (braket === '{' || braket === '[') {
+    multiplier[0] *= letterValues[braket]
+    multiplier[1] = true
+    return true
+  }
+  return false
+}
+function isClosedBraket(braket, multiplier) {
+  if (braket === '}' || braket === ']') {
+    multiplier[0] /= letterValues[braket]
+    if (multiplier[0] === 1) {
+      multiplier[1] = false
     }
-    if (letter === '{' || letter === '[') {
-      mupliplyer *= letterValues[letter]
-      return
+    return true
+  }
+  return false
+}
+function isMultiplyerOpen(multiplier) {
+  return multiplier[1]
+}
+function getScore(word) {
+  let wordValue = 0
+  const multiplier = [1, false]
+  for (let i = 0; i < word.length; i++) {
+    // console.log(word[i])
+    if (isValidLetter(word[i])) {
+      // console.log('Invalid Exit')
+      return 0
     }
-    if (letter === '}' || letter === ']') {
-      if (mupliplyer > 1) {
-        mupliplyer /= letterValues[letter]
-        return
-      } else {
-        invalid = 1
-        return
+    if (isOpenBraket(word[i], multiplier)) {
+      continue
+    }
+    if (isMultiplyerOpen(multiplier)) {
+      if (isClosedBraket(word[i], multiplier)) {
+        continue
+      }
+    } else {
+      if (isClosedBraket(word[i], multiplier)) {
+        return 0
       }
     }
-    wordValue += letterValues[letter] * mupliplyer
-  })
-  if (mupliplyer !== 1 || invalid === 1) {
-    // console.log('exit here, code:' + invalid)
-    return 0
+    wordValue += letterValues[word[i]] * multiplier[0]
   }
   // console.log('Exit: ' + wordValue)
+  if (isMultiplyerOpen(multiplier)) {
+    return 0
+  }
   return wordValue
 }
+
+function scrabble(word) {
+  if (isValidWord(word)) {
+    return getScore(word.toUpperCase())
+  }
+  return 0
+}
+scrabble('{dog}')
 
 module.exports = scrabble

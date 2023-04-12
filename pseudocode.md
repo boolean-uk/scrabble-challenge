482a6647f34dab72ef185492e4dccaa50f9d8d8f
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-04-12 14:51:18 +0300

    Nikolaos Giagkinis complete reimplementation of core to expand for extension requirements - not complete

--- pseudocode.md
+++ pseudocode.md
@@ -1,13 +1,78 @@
 # Pseudocode
 
-#### Add the steps you need to take to implement the requirements for Scrabble Challenge
+### For core exercise
 
+1. Write a function which gets an input from parameters.
+
+2. Check the input is correct and if not will return 0.
+
+3. Then in a loop I will pass each letter in the inputted word and calculate the score which will be added to a sum. Based on the following table :
+
+    | Letter                        | Value  |
+    | ----                          |  ----  |
+    | A, E, I, O, U, L, N, R, S, T  |     1  |
+    | D, G                          |     2  |
+    | B, C, M, P                    |     3  |
+    | F, H, V, W, Y                 |     4  |
+    | K                             |     5  |
+    | J, X                          |     8  |
+    | Q, Z                          |     10 |
+
+4. And lastly I will print a message with the word and how much it scored.
+
+### For extension
+
+Comments: Apparently my regex aprouch actually makes the extension to be lot more diffuclt so I reimplemented the program to something maybe.. more expandable(?).
+
+How I imlpemented regex for the core:
+
+```js
+const inputRegex = /[^a-zA-Z]/g
+const letterValue1 = /[a,e,i,o,u,l,n,r,s,t]/i
+const letterValue2 = /[d,g]/i
+const letterValue3 = /[b,c,m,p]/i
+const letterValue4 = /[f,h,v,w,y]/i
+const letterValue5 = /[k]/i
+const letterValue8 = /[j,x]/i
+const letterValue10 = /[q,z]/i
+
+function scrabble(word) {
+  let sum = 0
+
+  if (word === null) {
+    return 0
+  } else if (inputRegex.test(word)) {
+    return 0
+  } else {
+    for (let i = 0; i < word.length; i++) {
+      if (letterValue1.test(word[i])) {
+        sum += 1
+      } else if (letterValue2.test(word[i])) {
+        sum += 2
+      } else if (letterValue3.test(word[i])) {
+        sum += 3
+      } else if (letterValue4.test(word[i])) {
+        sum += 4
+      } else if (letterValue5.test(word[i])) {
+        sum += 5
+      } else if (letterValue8.test(word[i])) {
+        sum += 8
+      } else if (letterValue10.test(word[i])) {
+        sum += 10
+      }
+    }
+
+    // return 'Word: "' + word + '" scores: ' + sum
+    return sum
+  }
+}
+
+// console.log(scrabble('cabbage'))
+
+module.exports = scrabble
 ```
-1.
 
-2.
-
-3.
-
-...etc
-```
+1. Break each step to different function, first checkInput() to check for wrong inputs.
+2. Then it's easy to check if the word will score double or triple score with doubleTripleWord() function and return the value of the multiplier.
+3. Then I will clear the input of any outer symbols if it has a multiplier.
+4. 
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,142 @@
-function scrabble() {
-  // write code here
+function scrabble(word) {
+  if (checkInput(word)) {
+    const wordMultiplier = doubleTripleWord(word)
+    if (wordMultiplier > 1) {
+      const arrWord = word.split('')
+      arrWord.shift()
+      arrWord.pop()
+      word = arrWord.join('')
+    }
+
+    const wordScore = scoreLetters(word)
+    return wordScore * wordMultiplier
+  } else {
+    return 0
+  }
 }
+
+function checkInput(word) {
+  const inputRegex = /[^a-zA-Z{}[\]]/gi
+
+  if (!word) {
+    return 0
+  } else if (inputRegex.test(word)) {
+    return 0
+  }
+
+  return 1
+}
+
+function doubleTripleWord(word) {
+  if (word[0] === '{' && word[word.length - 1] === '}') {
+    return 2
+  } else if (word[0] === '[' && word[word.length - 1] === ']') {
+    return 3
+  } else {
+    return 1
+  }
+}
+
+function scoreLetters(word) {
+  let sum = 0
+  let letterMultiplier = 1
+
+  for (let i = 0; i < word.length; i++) {
+    switch (word[i]) {
+      case '{':
+        if (word[i + 2] === '}') {
+          letterMultiplier = 2
+        } else {
+          return 0
+        }
+        break
+      case '[':
+        if (word[i + 2] === ']') {
+          letterMultiplier = 3
+        } else {
+          return 0
+        }
+        break
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+      case 'l':
+      case 'n':
+      case 'r':
+      case 's':
+      case 't':
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+      case 'L':
+      case 'N':
+      case 'R':
+      case 'S':
+      case 'T':
+        sum += 1 * letterMultiplier
+        break
+      case 'd':
+      case 'g':
+      case 'D':
+      case 'G':
+        sum += 2 * letterMultiplier
+        break
+      case 'b':
+      case 'c':
+      case 'm':
+      case 'p':
+      case 'B':
+      case 'C':
+      case 'M':
+      case 'P':
+        sum += 3 * letterMultiplier
+        break
+      case 'f':
+      case 'h':
+      case 'v':
+      case 'w':
+      case 'y':
+      case 'F':
+      case 'H':
+      case 'V':
+      case 'W':
+      case 'Y':
+        sum += 4 * letterMultiplier
+        break
+      case 'k':
+      case 'K':
+        sum += 5 * letterMultiplier
+        break
+      case 'j':
+      case 'x':
+      case 'J':
+      case 'X':
+        sum += 8 * letterMultiplier
+        break
+      case 'q':
+      case 'z':
+      case 'Q':
+      case 'Z':
+        sum += 10 * letterMultiplier
+        break
+      case '}':
+      case ']':
+        if (word[i - 2] === '[' || word[i - 2] === '{') {
+          letterMultiplier = 1
+        } else {
+          return 0
+        }
+        break
+    }
+  }
+
+  return sum
+}
+
+// console.log(scrabble('d{o}g'))
 
 module.exports = scrabble

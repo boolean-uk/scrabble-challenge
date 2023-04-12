fd2adf35d0e3fc17f53ee385fb6f43871d5625dd
Rose <rosevandergiessen@proton.me>
2023-04-12 12:06:01 +0200

    clean up code & standard criteria met

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,35 +1,34 @@
 function scrabble(word) {
 
-  let onePoint = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
-  let twoPoint = ['D', 'G']
-  let threePoint = ['B', 'C', 'M', 'P']
-  let fourPoint = ['F', 'H', 'V', 'W', 'Y']
-  let fivePoint = ['K']
-  let eightPoint = ['J', 'X']
-  let tenPoint = ['Q', 'Z']
+  let oneValue = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+  let twoValue = ['D', 'G']
+  let threeValue = ['B', 'C', 'M', 'P']
+  let fourValue = ['F', 'H', 'V', 'W', 'Y']
+  let fiveValue = ['K']
+  let eightValue = ['J', 'X']
+  let tenValue = ['Q', 'Z']
 
   let score = 0
 
   if (word === '' || word === undefined || word === null) {
-    score = 0
-    return score
+    return score = 0
   } else {
     let letters = word.split('')
     for (let i = 0; i < letters.length; i++) {
       let capitalLetter = letters[i].toUpperCase()
-      if (onePoint.includes(capitalLetter)) {
+      if (oneValue.includes(capitalLetter)) {
         score += 1
-      } else if (twoPoint.includes(capitalLetter)) {
+      } else if (twoValue.includes(capitalLetter)) {
         score += 2
-      } else if (threePoint.includes(capitalLetter)) {
+      } else if (threeValue.includes(capitalLetter)) {
         score += 3
-      } else if (fourPoint.includes(capitalLetter)) {
+      } else if (fourValue.includes(capitalLetter)) {
         score += 4
-      } else if (fivePoint.includes(capitalLetter)) {
+      } else if (fiveValue.includes(capitalLetter)) {
         score += 5
-      } else if (eightPoint.includes(capitalLetter)) {
+      } else if (eightValue.includes(capitalLetter)) {
         score += 8
-      } else if (tenPoint.includes(capitalLetter)) {
+      } else if (tenValue.includes(capitalLetter)) {
         score += 10
       }
     }

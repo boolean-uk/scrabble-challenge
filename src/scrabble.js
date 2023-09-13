ae7aad62c0be5ed4c223720719339b7e43b5b863
Chloe Zermatten <chloezermatten@gmail.com>
2023-09-13 15:20:16 +0200

    amended the function to meet the case-insensitive requierement

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,64 +1,69 @@
 function scrabble(word) {
-  // initiate the variable scrabbleScore. It will keep track of the scores obtained for each letter as we iterate through the word
+  // initiate the variable scrabbleScore. It will keep track of the scores obtained for each letter as we iterate through the word - need to use let and not const as its value will be reassigned with each iteration of the for loop
   let scrabbleScore = 0
-  // iterate through the word (could use an for loop, could try to use .forEach() provided it works on strings - need to check)
   // if the word is an empty string, return 0 points (and do not bother running the rest of the code)
   if (word === null) {
-    return 0
+    return scrabbleScore
   }
-  for (let i = 0; i < word.length; i++) { 
-    // check each individual letter against the various if criterias below
+  // if the word it not all in lowercase, some if statements will fail. Therefore, we need .toLowerCase()
+  const lowerCaseWord = word.toLowerCase()
+  // iterate through the word (could use an for loop, could try to use .forEach() provided it works on strings - need to check)
+  // check each individual letter against the various if criterias below
+  for (let i = 0; i < word.length; i++) {
     // if the letter is A, E, I, O, U, L, N, R, S, or T, value = 1
     if (
-      word[i] === 'a' ||
-      word[i] === 'e' ||
-      word[i] === 'i' ||
-      word[i] === 'o' ||
-      word[i] === 'u' ||
-      word[i] === 'l' ||
-      word[i] === 'r' ||
-      word[i] === 's' ||
-      word[i] === 't'
+      lowerCaseWord[i] === 'a' ||
+      lowerCaseWord[i] === 'e' ||
+      lowerCaseWord[i] === 'i' ||
+      lowerCaseWord[i] === 'o' ||
+      lowerCaseWord[i] === 'u' ||
+      lowerCaseWord[i] === 'l' ||
+      lowerCaseWord[i] === 'n' ||
+      lowerCaseWord[i] === 'r' ||
+      lowerCaseWord[i] === 's' ||
+      lowerCaseWord[i] === 't'
     ) {
       scrabbleScore += 1
     }
     // if the letter is D or G value = 2
-    if (word[i] === 'd' || word[i] === 'g') {
+    if (lowerCaseWord[i] === 'd' || lowerCaseWord[i] === 'g') {
       scrabbleScore += 2
     }
     // if the letter is B, C, M, P, value = 3
     if (
-      word[i] === 'b' ||
-      word[i] === 'c' ||
-      word[i] === 'm' ||
-      word[i] === 'p'
+      lowerCaseWord[i] === 'b' ||
+      lowerCaseWord[i] === 'c' ||
+      lowerCaseWord[i] === 'm' ||
+      lowerCaseWord[i] === 'p'
     ) {
       scrabbleScore += 3
     }
     // if the letter is F, H, V, W, Y, value = 4
     if (
-      word[i] === 'f' ||
-      word[i] === 'h' ||
-      word[i] === 'v' ||
-      word[i] === 'w' ||
-      word[i] === 'y'
+      lowerCaseWord[i] === 'f' ||
+      lowerCaseWord[i] === 'h' ||
+      lowerCaseWord[i] === 'v' ||
+      lowerCaseWord[i] === 'w' ||
+      lowerCaseWord[i] === 'y'
     ) {
       scrabbleScore += 4
     }
     // if the letter is K, value = 5
-    if (word[i] === 'k') {
+    if (lowerCaseWord[i] === 'k') {
       scrabbleScore += 5
     }
     // if the letter is J or X, value = 8
-    if (word[i] === 'j' || word[i] === 'x') {
+    if (lowerCaseWord[i] === 'j' || lowerCaseWord[i] === 'x') {
       scrabbleScore += 8
     }
     // if the letter is Q or Z, value = 10
-    if (word[i] === 'q' || word[i] === 'z') {
+    if (lowerCaseWord[i] === 'q' || lowerCaseWord[i] === 'z') {
       scrabbleScore += 10
     }
   }
   return scrabbleScore
 }
+
+scrabble('POTATO')
 
 module.exports = scrabble

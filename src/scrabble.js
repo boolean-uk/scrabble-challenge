338fe45cab0ee7d154539872a1a544cad6203d3a
Nazar Tymiv <nazartumiv@gmail.com>
2023-09-13 12:30:45 +0100

    Solved scrabble file

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,28 @@
-function scrabble() {
-  // write code here
+const scoreLibrary = [
+  { letters: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], score: 1 },
+  { letters: ['D', 'G'], score: 2 },
+  { letters: ['B', 'C', 'M', 'P'], score: 3 },
+  { letters: ['F', 'H', 'V', 'W', 'Y'], score: 4 },
+  { letters: ['K'], score: 5 },
+  { letters: ['J', 'X'], score: 8 },
+  { letters: ['Q', 'Z'], score: 10 }
+]
+
+const scrabble = (str = '') => {
+  if (typeof str !== 'string') return 0
+
+  const strLetters = str.toUpperCase().split('')
+  let scores = 0
+
+  strLetters.forEach((item) => {
+    scoreLibrary.forEach((scoreItem) => {
+      if (scoreItem.letters.includes(item)) {
+        scores += scoreItem.score
+      }
+    })
+  })
+
+  return scores
 }
 
 module.exports = scrabble

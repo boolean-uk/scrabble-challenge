398b455875c212681da5778c275b4b26def3783c
Chloe Zermatten <chloezermatten@gmail.com>
2023-09-13 17:42:39 +0200

    stored the letters in arrays based on how many points they carry

--- pseudocode.md
+++ pseudocode.md
@@ -2,12 +2,35 @@
 
 #### Add the steps you need to take to implement the requirements for Scrabble Challenge
 
+TO MEET THE EXTENDED CRITERIA
+
+Miscelleanous
 ```
-1.
+I'd probably be better of storing the letters in arrays (one for each score that can be obtained), and use that, rather than writing lengthy if statements
+```
 
-2.
+Double and Triple Letter
+```
+0. take in a word as a parameter and iterate through it
+1. find specific letters 
+2. enclose them in {}
+3. double their value
+4. find other specific letters
+5. enclose them in []
+6. triple their value
+```
+Double or Triple Word
 
-3.
-
-...etc
+```
+1. take in a word as a parameter
+2. enclose it in {}
+3. double its value
+ OR
+2. enclose it in []
+3. triple its value
+```
+Concise Code
+```
+My best guess is that scrabble will need to work a bit like 'occupancy' did in softplay - all it does is to keep track of stuff
+3. 
 ```
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,69 +1,43 @@
+let scrabbleScore
+const lettersWorthOne = ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
+const lettersWorthTwo = ['d', 'g']
+const lettersWorthThree = ['b', 'c', 'm', 'p']
+const lettersWorthFour = ['f', 'h', 'v', 'w', 'y']
+const lettersWorthFive = ['k']
+const lettersWorthEight = ['j', 'x']
+const lettersWorthTen = ['q', 'z']
+
 function scrabble(word) {
-  // initiate the variable scrabbleScore. It will keep track of the scores obtained for each letter as we iterate through the word - need to use let and not const as its value will be reassigned with each iteration of the for loop
-  let scrabbleScore = 0
-  // if the word is an empty string, return 0 points (and do not bother running the rest of the code)
+  scrabbleScore = 0
   if (word === null) {
     return scrabbleScore
   }
-  // if the word it not all in lowercase, some if statements will fail. Therefore, we need .toLowerCase()
   const lowerCaseWord = word.toLowerCase()
-  // iterate through the word (could use an for loop, could try to use .forEach() provided it works on strings - need to check)
-  // check each individual letter against the various if criterias below
+
   for (let i = 0; i < word.length; i++) {
-    // if the letter is A, E, I, O, U, L, N, R, S, or T, value = 1
-    if (
-      lowerCaseWord[i] === 'a' ||
-      lowerCaseWord[i] === 'e' ||
-      lowerCaseWord[i] === 'i' ||
-      lowerCaseWord[i] === 'o' ||
-      lowerCaseWord[i] === 'u' ||
-      lowerCaseWord[i] === 'l' ||
-      lowerCaseWord[i] === 'n' ||
-      lowerCaseWord[i] === 'r' ||
-      lowerCaseWord[i] === 's' ||
-      lowerCaseWord[i] === 't'
-    ) {
+    if (lettersWorthOne.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 1
     }
-    // if the letter is D or G value = 2
-    if (lowerCaseWord[i] === 'd' || lowerCaseWord[i] === 'g') {
+    if (lettersWorthTwo.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 2
     }
-    // if the letter is B, C, M, P, value = 3
-    if (
-      lowerCaseWord[i] === 'b' ||
-      lowerCaseWord[i] === 'c' ||
-      lowerCaseWord[i] === 'm' ||
-      lowerCaseWord[i] === 'p'
-    ) {
+    if (lettersWorthThree.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 3
     }
-    // if the letter is F, H, V, W, Y, value = 4
-    if (
-      lowerCaseWord[i] === 'f' ||
-      lowerCaseWord[i] === 'h' ||
-      lowerCaseWord[i] === 'v' ||
-      lowerCaseWord[i] === 'w' ||
-      lowerCaseWord[i] === 'y'
-    ) {
+    if (lettersWorthFour.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 4
     }
-    // if the letter is K, value = 5
-    if (lowerCaseWord[i] === 'k') {
+    if (lettersWorthFive.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 5
     }
-    // if the letter is J or X, value = 8
-    if (lowerCaseWord[i] === 'j' || lowerCaseWord[i] === 'x') {
+    if (lettersWorthEight.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 8
     }
-    // if the letter is Q or Z, value = 10
-    if (lowerCaseWord[i] === 'q' || lowerCaseWord[i] === 'z') {
+    if (lettersWorthTen.includes(lowerCaseWord[i]) === true) {
       scrabbleScore += 10
     }
   }
   return scrabbleScore
 }
-
-// not sure if this is the most elegant solution, but it seems to be a solution
 
 module.exports = scrabble

20d502b99bde79984dddf1da6db90cd9df0d033c
Rebecca Noy <rebecca.noy@gmx.de>
2023-09-14 11:28:24 +0200

    Finally un-tangled brain-knot

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,6 @@
 function scrabble(word) {
 
-  if (typeof word !== "string" || word === null || word === undefined || word.length === 0 || !!word.match(/[^A-Za-z\[\]\{\}]/)) return 0
+  if (typeof word !== "string" || word === null || word === undefined || word.length === 0 || !!word.match(/[^A-Za-z\[\]\{\}]/g)) return 0
 
   const charValue = {
     A:1, B:3, C:3, D:2, E:1, F:4, G:2, H:4, I:1, J:8, K:5, L:1, M:3, N:1, O:1, P:3, Q:10, R:1, S:1, T:1, U:1, V:4, W:4, X:8, Y:4, Z:10
@@ -10,8 +10,8 @@ function scrabble(word) {
     current: 1
   }
 
-  charFactorChangeTrigger = (char, index) => {
-    if (!!char.match(/[\[\]\{\}]/) && index !== 0 && index !== charArr.length-1) {
+  charFactorChangeTrigger = (char) => {
+    if (!!char.match(/[\[\]\{\}]/)) {
       switch (char) {
         case "{":
           updateCharFactor(2)
@@ -20,17 +20,20 @@ function scrabble(word) {
           updateCharFactor(3)
           break;
         case "}":
+          updateCharFactor(1/2)
+          break;
         case "]":
-          updateCharFactor(charFactor.previous)
+          updateCharFactor(1/3)
           break;
       }
+      console.log("changed Factor", charFactor.previous, "->", charFactor.current)
     }
     return 0 // this is just so it will do its thing but doesn't affect the addition
   }
 
   updateCharFactor = (newFactor) => {
     charFactor.previous = charFactor.current;
-    charFactor.current = newFactor
+    charFactor.current *= newFactor
   }
 
   // determine the word factor
@@ -49,9 +52,9 @@ function scrabble(word) {
   let wordValue = 0
   word.length > 0 ? charArr = word.split("") : wordValue = 0
 
-  charArr.length > 0 ? charArr.forEach((char, index) => wordValue += (charFactorChangeTrigger(char, index) + getValue(char)) * charFactor.current) : wordValue = 0
+  charArr.length > 0 ? charArr.forEach((char) => wordValue += (charFactorChangeTrigger(char) + getValue(char)) * charFactor.current) : wordValue = 0
   
-  return charFactor.current === 1 ? wordValue * wordFactor(word) : 0
+  return charFactor.current === 1 ? wordValue : 0
 }
 
 module.exports = scrabble

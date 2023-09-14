3046512238d01c230fb5998c3bdc6c1c6da06f10
Nazar Tymiv <nazartumiv@gmail.com>
2023-09-14 14:57:57 +0100

    Solved EXTENSIONS file it was crazy

--- src/scrabble.js
+++ src/scrabble.js
@@ -11,16 +11,153 @@ const scoreLibrary = [
 const scrabble = (str = '') => {
   if (typeof str !== 'string') return 0
 
-  const strLetters = str.toUpperCase().split('')
   let scores = 0
 
-  strLetters.forEach((item) => {
-    scoreLibrary.forEach((scoreItem) => {
-      if (scoreItem.letters.includes(item)) {
-        scores += scoreItem.score
+  const splitStr = str.split('')
+
+  if (
+    (splitStr.includes('{') && splitStr.includes('}')) ||
+    (splitStr.includes('[') && splitStr.includes(']')) ||
+    /^[A-Z]*$/.test(str.toUpperCase())
+  ) {
+    let newStr = str
+
+    // MULTIPLE BRACKETS
+    if (
+      splitStr.includes('{') &&
+      splitStr.includes('}') &&
+      splitStr.includes('[') &&
+      splitStr.includes(']')
+    ) {
+      let curlyBrackets = str.toUpperCase().match(/{([^}]+)}/g)[0]
+      let squareBrackets = str.toUpperCase().match(/\[(.*?)\]/g)[0]
+
+      if (curlyBrackets.length > squareBrackets.length) {
+        curlyBrackets = curlyBrackets.replace(squareBrackets, '')
+
+        squareBrackets
+          .slice(0, -1)
+          .slice(1)
+          .split('')
+          .forEach((item) => {
+            item.split('').forEach((value) => {
+              scoreLibrary.forEach((scoreItem) => {
+                if (scoreItem.letters.includes(value)) {
+                  scores += scoreItem.score * 3
+                }
+              })
+            })
+          })
+
+        scores = scores * 2
+
+        curlyBrackets
+          .slice(0, -1)
+          .slice(1)
+          .split('')
+          .forEach((item) => {
+            console.log('square', item)
+            item.split('').forEach((value) => {
+              scoreLibrary.forEach((scoreItem) => {
+                if (scoreItem.letters.includes(value))
+                  scores += scoreItem.score * 2
+              })
+            })
+          })
+      } else {
+        squareBrackets = squareBrackets.replace(curlyBrackets, '')
+
+        curlyBrackets
+          .slice(0, -1)
+          .slice(1)
+          .split('')
+          .forEach((item) => {
+            item.split('').forEach((value) => {
+              scoreLibrary.forEach((scoreItem) => {
+                if (scoreItem.letters.includes(value)) {
+                  scores += scoreItem.score * 2
+                }
+              })
+            })
+          })
+
+        scores = scores * 3
+
+        squareBrackets
+          .slice(0, -1)
+          .slice(1)
+          .split('')
+          .forEach((item) => {
+            console.log('square', item)
+            item.split('').forEach((value) => {
+              scoreLibrary.forEach((scoreItem) => {
+                if (scoreItem.letters.includes(value))
+                  scores += scoreItem.score * 3
+              })
+            })
+          })
       }
-    })
-  })
+
+      return scores
+    }
+
+    // CURLY BRACKETS
+    if (splitStr.includes('{') && splitStr.includes('}')) {
+      const curlyBrackets = str.toUpperCase().match(/{([^}]+)}/g)
+
+      curlyBrackets
+        .map((item) => {
+          newStr = newStr.toUpperCase().replace(item, '')
+
+          return item.slice(0, -1).slice(1)
+        })
+        .forEach((item) => {
+          item.split('').forEach((value) => {
+            scoreLibrary.forEach((scoreItem) => {
+              if (scoreItem.letters.includes(value)) {
+                scores += scoreItem.score * 2
+              }
+            })
+          })
+        })
+    }
+
+    // SQUARE BRACKETS
+    if (splitStr.includes('[') && splitStr.includes(']')) {
+      const squareBrackets = str.toUpperCase().match(/\[(.*?)\]/g)
+
+      squareBrackets
+        .map((item) => {
+          newStr = newStr.toUpperCase().replace(item, '')
+
+          return item.slice(0, -1).slice(1)
+        })
+        .forEach((item) => {
+          item.split('').forEach((value) => {
+            scoreLibrary.forEach((scoreItem) => {
+              if (scoreItem.letters.includes(value)) {
+                scores += scoreItem.score * 3
+              }
+            })
+          })
+        })
+    }
+
+    // NO BRACKETS
+    if (/^[A-Z]*$/.test(newStr.toUpperCase())) {
+      const strLetters = newStr.toUpperCase().split('')
+
+      strLetters.forEach((item) => {
+        scoreLibrary.forEach((scoreItem) => {
+          if (scoreItem.letters.includes(item)) {
+            scores += scoreItem.score
+          }
+        })
+      })
+    }
+  } else {
+    return 0
+  }
 
   return scores
 }

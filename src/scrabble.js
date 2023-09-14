e0f20508852b8399615176350f9608be60055060
Kye <NovusAa12@proton.me>
2023-09-14 20:09:32 +0100

    extension tests passed (not elegant but it works)

--- src/scrabble.js
+++ src/scrabble.js
@@ -2,31 +2,80 @@ const { letters } = require('./letter.js')
 
 const scrabble = (word) => {
   let output = 0
-  const scrabbleArr = []
+  let mulitplier = 1
+  // Arrays to test for valid input
+  const checkerArr = []
+  const checkerArr2 = []
 
+  // Checking for any number in input
   function checkString(string) {
     return /^[0-9]*$/.test(string)
   }
 
   const stringContainsNum = checkString(word)
 
-  if (typeof word === 'string' && stringContainsNum === false) {
+  // Multiple checks for valid input
+  if (
+    typeof word === 'string' &&
+    stringContainsNum === false &&
+    /^[a-zA-Z{}[\]]*$/.test(word)
+  ) {
     for (let i = 0; i < word.length; i++) {
-      scrabbleArr.push(word[i].toLowerCase())
-    }
+      const letter = word[i].toLowerCase()
 
-    for (const letter of scrabbleArr) {
-      if (letter in letters) {
-        output += letters[letter]
+      // Checking for ({,[), if true then multiplier applied
+      // Adds check to checkerArr until next iteration
+      // If the accompanying (},]) are present, check is removed &
+      // resets multiplier to 1
+      // If check is still there, input is invalid and Line 66 returns false
+      if (letter === '{') {
+        mulitplier *= 2
+        checkerArr.push('}')
+      } else if (letter === '}') {
+        mulitplier /= 2
+        checkerArr.pop()
+      } else if (letter === '[') {
+        mulitplier *= 3
+        checkerArr.push(']')
+      } else if (letter === ']') {
+        mulitplier /= 3
+        checkerArr.pop()
+      } else {
+        for (const key in letters) {
+          if (key.includes(letter)) {
+            output += letters[key] * mulitplier
+            break
+          }
+        }
       }
     }
-  } else if (stringContainsNum === true) {
+
+    // Checking if there's a lonely ] or }
+    for (const idx of word) {
+      if (idx === '{' || idx === '[') {
+        checkerArr2.push(idx)
+      } else if (idx === '}' && checkerArr2.pop() !== '{') {
+        console.log('Error: Input incorrect, please open AND close {} or []')
+        output = 0
+      } else if (idx === ']' && checkerArr2.pop() !== '[') {
+        console.log('Error: Input incorrect, please open AND close {} or []')
+        output = 0
+      }
+    }
+  } else {
     console.log(
       "Error: Please input a valid WORD! Example: 'Animal', 'Rhinoceros', 'Drink' etc.."
     )
   }
-  return output
+  // Checking if checkerArr is empty, if not guard clause activates
+  if (checkerArr.length === 0) {
+    return output
+  } else {
+    console.log('Error: Input incorrect, please open AND close {} or []')
+    return (output = 0)
+  }
 }
 
-console.log(scrabble('d{o}g'))
+console.log(scrabble('hel}p'))
+
 module.exports = scrabble

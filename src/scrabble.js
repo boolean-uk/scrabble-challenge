ddea2530b7b32010dabdb1d2d470a7b603693dc9
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-09-15 11:54:33 +0200

    scrabble challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -27,59 +27,30 @@ const eachLetterValues = {
   Z: 10
 }
 
-// Function to calculate the score for a letter with double or triple modifiers
-function calculateLetterScore(letter, modifier) {
-  const letterValue = eachLetterValues[letter] || 0
-  if (modifier === '{}') {
-    return letterValue * 2
-  } else if (modifier === '[]') {
-    return letterValue * 3
-  } else {
-    return letterValue
-  }
-}
-
-// Function to calculate the score for a word with double or triple word modifiers
-function calculateWordScore(word, modifier) {
-  let wordScore = 0
-  for (let i = 0; i < word.length; i++) {
-    const letter = word[i]
-    const letterModifier =
-      word[i - 1] === '{' ? '{}' : word[i - 1] === '[' ? '[]' : ''
-    wordScore += calculateLetterScore(letter, letterModifier)
-  }
-  if (modifier === '{}') {
-    return wordScore * 2
-  } else if (modifier === '[]') {
-    return wordScore * 3
-  } else {
-    return wordScore
-  }
+function somethingElse() {
+  console.log('This is the console log from somethingElse')
 }
 
 function scrabble(word) {
-  if (!word) return 0
+  console.log('What is the word?', word)
 
+  // Calculate the score for each letter in the word to get each value so therefore we use the for loop
+  let totalScoreValue = 0
+  // check the input word is null or empty
+  if (word === null || word.trim().length === 0) {
+    return totalScoreValue
+  }
   // Convert the word to uppercase to handle both uppercase and lowercase letters
   word = word.toUpperCase()
-
-  let totalScoreValue = 0
-  let wordModifier = ''
-
   for (let i = 0; i < word.length; i++) {
-    const char = word[i]
-    if (char === '{' || char === '[') {
-      // Detect word modifiers
-      wordModifier = char + char
-    } else if (char === '}' || char === ']') {
-      // Calculate word score and reset modifier
-      totalScoreValue += calculateWordScore(word.substring(0, i), wordModifier)
-      wordModifier = ''
-    }
+    const letter = word[i]
+    // Use the eachLetterValues object to get the value for the letter
+    const letterValue = eachLetterValues[letter] || 0
+    // Add the letter score to the total score
+    totalScoreValue += letterValue
   }
 
-  // Calculate the score for the remaining part of the word
-  totalScoreValue += calculateWordScore(word, wordModifier)
+  somethingElse() // Call the somethingElse function
 
   return totalScoreValue
 }
@@ -91,7 +62,7 @@ console.log(scrabble('a')) // should return 1
 console.log(scrabble('f')) // should return 4
 console.log(scrabble('street')) // should return 6
 console.log(scrabble('quirky')) // should return 22
-console.log(scrabble('{OXYPHENBUTAZONE}')) // should return 135
-console.log(scrabble('cabbage')) // should return 14
+console.log(scrabble('OXYPHENBUTAZONE')) // should return 41
+console.log(scrabble('cabbage'))
 
 module.exports = scrabble

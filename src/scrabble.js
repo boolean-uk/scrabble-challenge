e0b719ccd0f08d0fca77bf8c7eece51429f546e2
Mobeen <muhammadmobeen1@hotmail.com>
2023-09-15 11:46:09 +0200

    Still Working on it

--- src/scrabble.js
+++ src/scrabble.js
@@ -3,3 +3,7 @@ function scrabble() {
 }
 
 module.exports = scrabble
+
+// Already talked to Jules :)
+// I am revising the previuos work we did so that I can easily do this exersice. It will be done over this weekend or maybe 
+// TODAY

8eb9a03f84ce4cc96362ec4181677829c686651d
Husnu Koci <husnukoci94@gmail.com>
2023-09-18 09:18:52 +0300

    Core is done, Working on extensions

--- src/scrabble.js
+++ src/scrabble.js
@@ -16,54 +16,55 @@
 // eslint-disable-next-line no-unused-vars
 // For input if write null we need to get 0
 function scrabble(word) {
-  // eslint-disable-next-line use-isnan, valid-typeof
-  if (typeof word !== 'string' || word === '' || word === ' \t\n') {
-    return 0
+    // eslint-disable-next-line use-isnan, valid-typeof
+    if (typeof word !== 'string' || word === '' || word === ' \t\n') {
+      return 0
+    }
+    // Define an object to map each letter to its corresponding score
+    const letterScores = {
+      A: 1,
+      E: 1,
+      I: 1,
+      O: 1,
+      U: 1,
+      L: 1,
+      N: 1,
+      R: 1,
+      S: 1,
+      T: 1,
+      D: 2,
+      G: 2,
+      B: 3,
+      C: 3,
+      M: 3,
+      P: 3,
+      F: 4,
+      H: 4,
+      V: 4,
+      W: 4,
+      Y: 4,
+      K: 5,
+      J: 8,
+      X: 8,
+      Q: 10,
+      Z: 10
+    }
+  
+    // Initialize a variable to store the total score
+    let score = 0
+  
+    // Loop over each letter in the word
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i].toUpperCase()
+      // Convert the letter to uppercase for case-insensitivity
+      console.log(letter)
+      const number = letterScores[letter] // Look up the score for this letter in the letterScores object, or 0 if not found
+      score += number // score = score + number// Add the letter's score to the total score
+    }
+  
+    // Return the total score
+    return score
   }
-  // Define an object to map each letter to its corresponding score
-  const letterScores = {
-    A: 1,
-    E: 1,
-    I: 1,
-    O: 1,
-    U: 1,
-    L: 1,
-    N: 1,
-    R: 1,
-    S: 1,
-    T: 1,
-    D: 2,
-    G: 2,
-    B: 3,
-    C: 3,
-    M: 3,
-    P: 3,
-    F: 4,
-    H: 4,
-    V: 4,
-    W: 4,
-    Y: 4,
-    K: 5,
-    J: 8,
-    X: 8,
-    Q: 10,
-    Z: 10
-  }
-
-  // Initialize a variable to store the total score
-  let score = 0
-
-  // Loop over each letter in the word
-  for (let i = 0; i < word.length; i++) {
-    const letter = word[i].toUpperCase()
-    // Convert the letter to uppercase for case-insensitivity
-    console.log(letter)
-    const number = letterScores[letter] // Look up the score for this letter in the letterScores object, or 0 if not found
-    score += number // score = score + number// Add the letter's score to the total score
-  }
-
-  // Return the total score
-  return score
-}
-console.log(scrabble('spiderman'))
-module.exports = scrabble
+  // K
+  console.log(scrabble('spiderman'))
+  module.exports = scrabble

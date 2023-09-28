dd97db4b560f43e6229fce80a9edec8d5b8f47f8
Daniel <dadefataki@gmail.com>
2023-09-28 17:13:00 +0200

    again

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,32 +1,32 @@
 function scrabble() {}
       // write code here
 const Libraryscores = {
-  A: 1
-  E: 1
-  I: 1
-  O: 1
-  U: 1
-  L: 1
-  N: 1
-  R: 1
-  S: 1 
-  T: 1 
-  D: 2
-  G: 2
-  C: 3
-  P: 3
-  M: 3
-  B: 3
-  W: 4
-  Y: 4
-  H: 4
-  F: 4
-  V: 4
-  K: 5
-  J: 8
-  X: 8
-  Q: 10
-  Z: 10
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1, 
+  T: 1, 
+  D: 2,
+  G: 2,
+  C: 3,
+  P: 3,
+  M: 3,
+  B: 3,
+  W: 4,
+  Y: 4,
+  H: 4,
+  F: 4,
+  V: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
 }
 
 const scrubble (LibraryScore = str = '') => {
@@ -42,7 +42,7 @@ for (let i = 0; i < word.lenght; 1++) {
 return sum
 }
 
-console.log(scrubble(null))
+console.log(scrubble())
 
 
 module.exports = scrabble

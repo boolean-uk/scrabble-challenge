b6270bb5184f1804b079d8aa20ef3d4128d75cac
Daniel <dadefataki@gmail.com>
2023-10-31 15:44:47 +0100

    redid the challange

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,48 +1,31 @@
-function scrabble() {}
-      // write code here
-const Libraryscores = {
-  A: 1,
-  E: 1,
-  I: 1,
-  O: 1,
-  U: 1,
-  L: 1,
-  N: 1,
-  R: 1,
-  S: 1, 
-  T: 1, 
-  D: 2,
-  G: 2,
-  C: 3,
-  P: 3,
-  M: 3,
-  B: 3,
-  W: 4,
-  Y: 4,
-  H: 4,
-  F: 4,
-  V: 4,
+const LibraryScores = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  C: 3, M: 3, P: 3, B: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
   K: 5,
-  J: 8,
-  X: 8,
-  Q: 10,
-  Z: 10,
+  J: 8, X: 8,
+  Q: 10, Z: 10,
+};
+
+function scrabble(word) {
+  if (typeof word !== 'string') {
+    return 0;
+  }
+
+  const uppercaseWord = word.toUpperCase();
+  let sum = 0;
+
+  for (let i = 0; i < uppercaseWord.length; i++) {
+    const letter = uppercaseWord[i];
+    if (LibraryScores[letter]) {
+      sum += LibraryScores[letter];
+    }
+  }
+
+  return sum;
 }
 
-const scrubble (LibraryScore = str = '') => {
-  if (Libraryscores === '' || Libraryscores === null || Libraryscores === ' \t\n') {
-    return 0
-}
-const word = Libraryscores.toUppercase()
-let sum = 0
+console.log(scrabble("example")); // Replace with your desired word
 
-for (let i = 0; i < word.lenght; 1++) {
-  sum += [Libraryscores[i]]
-}
-return sum
-}
-
-console.log(scrubble())
-
-
-module.exports = scrabble
+module.exports = scrabble;

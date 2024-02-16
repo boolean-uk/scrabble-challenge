9cecb492a7e8829d78b30d2319f3163d5ee2449d
spectraldesign <spectraldesign1@gmail.com>
2024-02-16 16:07:15 +0100

    Extensions

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,13 +1,3 @@
-/*
-A, E, I, O, U, L, N, R, S, T = 	1
-D, G =	2
-B, C, M, P =	3
-F, H, V, W, Y = 4
-K = 5
-J, X = 8
-Q, Z = 10
-*/
-
 const letterMap = {
   A: 1,
   E: 1,
@@ -37,15 +27,60 @@ const letterMap = {
   Z: 10
 }
 
+const multiplierList = ['{', '}', '[', ']']
+
 function scrabble(input) {
   if (!input || input === '') {
     return 0
   }
   input = input.trim()
   const letters = input.toUpperCase().split('')
-  return letters.reduce((acc, letter) => {
-    return acc + letterMap[letter]
-  }, 0)
+  const score = calculateScore(letters)
+  return score
 }
+
+function calculateScore(letters) {
+  let score = 0
+  let scoreMultiplier = 1
+  const multiplierStack = []
+  for (let i = 0; i < letters.length; i++) {
+    if (!isValidLetter(letters[i])) return 0
+    if (letters[i] === '{') {
+      scoreMultiplier = scoreMultiplier * 2
+      multiplierStack.push('{')
+      continue
+    }
+    if (letters[i] === '[') {
+      scoreMultiplier = scoreMultiplier * 3
+      multiplierStack.push('[')
+      continue
+    }
+    if (letters[i] === '}') {
+      if (multiplierStack.pop() !== '{') return 0 // invalid multiplier
+      scoreMultiplier = scoreMultiplier / 2
+      continue
+    }
+    if (letters[i] === ']') {
+      if (multiplierStack.pop() !== '[') return 0 // invalid multiplier
+      scoreMultiplier = scoreMultiplier / 3
+      continue
+    }
+    score += letterMap[letters[i]] * scoreMultiplier
+  }
+  if (multiplierStack.length > 0) return 0
+  return score
+}
+
+function isValidLetter(letter) {
+  return letter in letterMap || multiplierList.includes(letter)
+}
+
+// function getLetterMultiplier(letters, index) {
+//   if (letters[index] === '}') return 1 / 2
+//   if (letters[index] === ']') return 1 / 3
+//   if (letters[index] === '{' && letters[index + 2] === '}') return 2
+//   if (letters[index] === '[' && letters[index + 2] === ']') return 3
+//   return 1
+// }
 
 module.exports = scrabble

00b6defb5cc3c38e375b86e6b9e5dd75e177735b
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-19 10:08:36 +0100

    Finish core criteria

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,17 @@
-function scrabble() {
+const SCRABBLE_LETTER_POINTS = require('./scrabblePoints')
+
+function scrabble(word) {
   // write code here
+  if (typeof word !== typeof '') {
+    return 0
+  }
+  let score = 0
+  const upperCaseWord = word.toUpperCase().replace(/\s/g, '')
+  console.log(upperCaseWord)
+  for (let i = 0; i < upperCaseWord.length; i++) {
+    score += SCRABBLE_LETTER_POINTS[upperCaseWord[i]]
+  }
+  return score
 }
 
 module.exports = scrabble

8b013ee52ee5299781049e58c745811d7c29a73c
Will Baxter <bax.will@googlemail.com>
2024-02-28 16:44:03 +0000

    Refactored and streamlined code

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,130 +1,99 @@
 const letterScores = {
-  a: 1,
-  b: 3,
-  c: 3,
-  d: 2,
-  e: 1,
-  f: 4,
-  g: 2,
-  h: 4,
-  i: 1,
-  j: 8,
-  k: 5,
-  l: 1,
-  m: 3,
-  n: 1,
-  o: 1,
-  p: 3,
-  q: 10,
-  r: 1,
-  s: 1,
-  t: 1,
-  u: 1,
-  v: 4,
-  w: 4,
-  x: 8,
-  y: 4,
-  z: 10,
+  a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1,
+  j: 8, k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1,
+  s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10,
 }
-
-
-const validCheck = word => { 
-
-  for (let i = 0; i < word.length; i++) {
-      if (Object.keys(letterScores).includes(word[i]) || word[i] === '{' || word[i] === '}' || word[i] === '[' || word[i] ===']') {
-          switch(word[i]){
-              case '{': frontBraceCount++;
-              break;
-              case '}': backBraceCount++;
-              break;
-              case '[': frontSquareCount++;
-              break;
-              case ']': backSquareCount++
-              break;
-          }
-      } else {
-          return false
-      }
-  }
-
-  if (frontBraceCount !== backBraceCount || frontSquareCount !== backSquareCount){
-      return false
-  } else {
-      return true
-  }
-}      
-
-const getLetterScores = word => {
-  doubleLetterScore = 0
-  tripleLetterScore = 0
-  singleLetterScore = 0
-
-  for (let i = 0; i < word.length; i++){
-      if (word[i] === '{' || word[i] === '}' || word[i] === '[' || word[i] === ']'){
-          continue
-      }
-      if (word[i-1] === '{' && word[i+1] === '}') {
-          doubleLetterScore += (letterScores[word[i]]*2)
-      } else if (word[i-1] === '[' && word[i+1] === ']') {
-          tripleLetterScore += (letterScores[word[i]]*3)
-      } else {
-          singleLetterScore += letterScores[word[i]]
-      }
-  }
-}
-
-const multiWordCheck = word => {
-  for (let i = 0; i < word.length; i++){
-    if (word[i] === '{' && word[i+2] !== '}') {
-      doubleWordScore = true
-    }
-    if (word[i] === '[' && word[i+2] !== ']') {
-      tripleWordScore = true
-    }
-  }
-}
-
-const calculateFinalScore = () => {
-
-  totalLetterScore = singleLetterScore + doubleLetterScore + tripleLetterScore
-  
-  if (doubleWordScore && tripleWordScore) {
-      return totalLetterScore * 3 * 2
-  } else if (doubleWordScore){
-      return totalLetterScore * 2
-  } else if (tripleWordScore){
-      return totalLetterScore * 3
-  } 
-  return totalLetterScore
-}
-
 
 const scrabble = word => {
   if (word === null) {
     return 0
   }
-  variableReset()
-  lowerWord = word.toLowerCase()
-  if (validCheck(lowerWord)){
-      getLetterScores(lowerWord)
-      multiWordCheck(lowerWord)
-      return calculateFinalScore()
+  let lowerCaseWord = word.toLowerCase()
+
+  if (validWordCheck(lowerCaseWord)) {
+    let totalLetterScore = calculateLetterScores(lowerCaseWord)
+    doubleWordScore = determineDoubleWord(lowerCaseWord)
+    tripleWordScore = determineTripleWord(lowerCaseWord) 
+    let totalScore = calculateFinalScore(totalLetterScore, doubleWordScore, tripleWordScore)
+    return totalScore
   } else {
-      return 0
+    return 0
   }
 }
 
-const variableReset = () => {
-  singleLetterScore = 0
-  doubleLetterScore = 0
-  tripleLetterScore = 0
-  doubleWordScore = false
-  tripleWordScore = false
-  totalLetterScore = 0
-  backBraceCount = 0
-  frontSquareCount = 0
-  backSquareCount = 0
-  frontBraceCount = 0
+const validWordCheck = word => {
+  let frontBraceCount = 0
+  let backBraceCount = 0
+  let frontSquareCount = 0
+  let backSquareCount = 0
+
+  for (let i = 0; i < word.length; i++) {
+    if (Object.keys(letterScores).includes(word[i]) || word[i] === '{' || word[i] === '}' || word[i] === '[' || word[i] === ']') {
+      switch(word[i]){
+        case '{': frontBraceCount++; break;
+        case '}': backBraceCount++; break;
+        case '[': frontSquareCount++; break;
+        case ']': backSquareCount++; break;
+      }
+    } else {
+      return false
+    }
+  }
+
+  if (frontBraceCount !== backBraceCount || frontSquareCount !== backSquareCount) {
+    return false
+  } else {
+    return true
+  }
+}
+
+const calculateLetterScores = word => {
+  let singleLetterScore = 0
+  let doubleLetterScore = 0
+  let tripleLetterScore = 0
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === '{' || word[i] === '}' || word[i] === '[' || word[i] === ']') {
+      continue
+    }
+    if (word[i-1] === '{' && word[i+1] === '}') {
+      doubleLetterScore += (letterScores[word[i]] * 2)
+    } else if (word[i-1] === '[' && word[i+1] === ']') {
+      tripleLetterScore += (letterScores[word[i]] * 3)
+    } else {
+      singleLetterScore += letterScores[word[i]]
+    }
+  }
+  return singleLetterScore + doubleLetterScore + tripleLetterScore
+}
+
+const determineDoubleWord = word => {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === '{' && word[i+2] !== '}') {
+      return true
+    }
+  }
+  return false
+}
+
+const determineTripleWord = word => {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === '[' && word[i+2] !== ']') {
+      return true
+    }
+  }
+  return false
+} 
+
+const calculateFinalScore = (letterScore, doubleScore, tripleScore) => {
+  if (doubleScore && tripleScore) {
+    return letterScore * 3 * 2
+  } else if (doubleScore) {
+    return letterScore * 2
+  } else if (tripleScore) {
+    return letterScore * 3
+  }
+  return letterScore
 }
 
 module.exports = scrabble

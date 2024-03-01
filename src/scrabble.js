f7498b560d5a68172233a593a4d8d06670adcd63
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-03-01 05:21:43 +0000

    Scrabble done

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,47 @@
-function scrabble() {
-  // write code here
+function scrabble(word) {
+  if (!word || word.trim() === '') {
+    return 0
+  }
+
+  const scores = {
+    A: 1,
+    E: 1,
+    I: 1,
+    O: 1,
+    U: 1,
+    L: 1,
+    N: 1,
+    R: 1,
+    S: 1,
+    T: 1,
+    D: 2,
+    G: 2,
+    B: 3,
+    C: 3,
+    M: 3,
+    P: 3,
+    F: 4,
+    H: 4,
+    V: 4,
+    W: 4,
+    Y: 4,
+    K: 5,
+    J: 8,
+    X: 8,
+    Q: 10,
+    Z: 10
+  }
+
+  let score = 0
+  const upperWord = word.toUpperCase()
+  for (let i = 0; i < upperWord.length; i++) {
+    const letter = upperWord[i]
+    if (scores.hasOwnProperty(letter)) {
+      score += scores[letter]
+    }
+  }
+
+  return score
 }
 
 module.exports = scrabble

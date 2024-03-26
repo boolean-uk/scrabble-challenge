756b73117dcaf41e82582c63ed64ac95bc28631a
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-26 17:06:50 +0100

    ran eslint

--- src/scrabble.js
+++ src/scrabble.js
@@ -11,11 +11,18 @@ function scrabble(word) {
   if (!!word === false || !!word.match(/[^A-Za-z\[\]\{\}]/g)) return 0
 
   let wordValue = 0
-  word.length > 0 ? charArr = word.split("") : wordValue = 0
+  word.length > 0 ? (charArr = word.split('')) : (wordValue = 0)
 
-  charArr.length > 0 ? charArr.forEach((char) => wordValue += (charFactorChangeTrigger(char) + getValue(char)) * charFactor.current) : wordValue = 0
+  charArr.length > 0
+    ? charArr.forEach(
+        (char) =>
+          (wordValue +=
+            (charFactorChangeTrigger(char) + getValue(char)) *
+            charFactor.current)
+      )
+    : (wordValue = 0)
 
-  return (charFactor.current === 1 && charFactor.valid === true) ? wordValue : 0
+  return charFactor.current === 1 && charFactor.valid === true ? wordValue : 0
 }
 
 module.exports = scrabble

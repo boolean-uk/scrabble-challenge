47fbe8e55c16fbbe5f7a63e3730eb0ab7191ed4b
James Canterbury <jamesdcanterbury@protonmail.com>
2024-04-30 16:07:41 +0100

    core completed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,31 @@
-function scrabble() {
-  // write code here
+const { letters, scores } = require('./scrabbleinfo.js')
+
+function scrabble(word) {
+  let total = 0
+  if (typeof word !== 'string') {
+    return total
+  }
+  word = word.toLowerCase()
+  for (let i = 0; i < word.length; i++) {
+    const index = letters.indexOf(word[i])
+    const score = scores[index]
+    if (score === undefined) {
+      total = 0
+      break
+    }
+    total += score
+  }
+  /*
+  if (word[0] === '{' && word[word.length - 1] === '}') {
+    total += total
+  } else if (word[0] === '[' && word[word.length - 1] === ']') {
+    total = total * 3
+  }
+  */
+  console.log(total)
+  return total
 }
+
+scrabble('{monkey}')
 
 module.exports = scrabble

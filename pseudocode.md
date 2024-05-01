d7740221eff65ad9bf14c3efebaf5708a1f595e2
gent009 <167322651+gent009@users.noreply.github.com>
2024-05-01 15:33:41 +0100

    Scrabble

--- pseudocode.md
+++ pseudocode.md
@@ -3,11 +3,32 @@
 #### Add the steps you need to take to implement the requirements for Scrabble Challenge
 
 ```
-1.
+1.Mapping of letters to their Scrabble values
 
-2.
+2.Define a function called scrabble(word) that takes a string as an argument.
 
-3.
+3.Loop over each character in the word.
+ a. Check if the character is "{"
+  -set letter modifier 2 (double letter score )
+  -increment the index to point to the nexr character
+  -get the letter at the current index
+  - Increment score 
+  -Increment index to skip to the closing brace "}"
+ b. Check if the character is "["
+ set letter modifier 3 (triple letter score )
+  -increment the index to point to the next character
+  -get the letter at the current index
+  - Increment score 
+  -Increment index to skip to the closing brace "]"
+c.Check if the character is "(":
+-Set word_multiplier to 2.
+d.Check if the character is "<":
+-Set word_multiplier to 3.
+e. Otherwise add the regular letter score
+
+4.Return the total score multiplied by the world multiplier:
+return score * word_multiplier
+5. call the function scrabble
 
 ...etc
 ```
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,49 @@
-function scrabble() {
-  // write code here
+
+const letterValues = {
+  'A': 1, 'E':1, 'I':1, 'O':1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+  'D': 2, 'G': 2, 
+  'B': 3, 'C': 3, 'M': 3, 'P': 3,
+  'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+  'K': 5,
+  'J': 8, 'X': 8,
+  'Q': 10, 'Z': 10,
 }
 
-module.exports = scrabble
+function scrabble(word) {
+  // write code here
+  if (word == null){
+    return 0;
+  }
+  let score = 0;
+  let wordMultiplier =1;
+
+  for (let i = 0; i < word.length; i++){
+    const char = word[i];
+
+    if (char === '{' || char === '[') {
+      let endChar = char === '{' ? '}': ']';
+      let modifier = char === '{'? 2 : 3;
+      i++;
+      const letter = word[i];
+      if (!letter) break;
+      score += (letterValues[letter.toUpperCase()] || 0) * modifier;
+      i++;
+      while (i < word.length && word[i] !== endChar)
+      i++;
+    }
+    else if (char === '(') { 
+
+      wordMultiplier = 2;
+    }
+    else if (char === '['){
+      wordMultiplier = 3;
+    }
+    else {
+      score += letterValues[char.toUpperCase()] || 0;
+
+    }
+  }
+  return score * wordMultiplier;
+}
+
+module.exports = scrabble;

77e75538fb8f12a3dabe5793f32894da1942ec07
Olaoluwa Oyebode <olaolumcpaul@gmail.com>
2024-05-02 14:50:09 +0100

    Olaoluwa Oyebode_Push Request

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,24 @@
 function scrabble(word) {
   if (!word || typeof word !== 'string' || word.trim() === '') return 0
 
+  if (/[^a-zA-Z{}\[\]]/.test(word)) return 0
+
+  const stack = []
+  const pairs = { '{': '}', '[': ']' }
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i]
+    if (char === '{' || char === '[') {
+      stack.push(char)
+    } else if (char === '}' || char === ']') {
+      if (stack.length === 0 || pairs[stack.pop()] !== char) {
+        return 0
+      }
+    }
+  }
+
+  if (stack.length > 0) return 0
+
   const scores = {
     A: 1,
     E: 1,
@@ -30,26 +48,45 @@ function scrabble(word) {
     Z: 10
   }
 
+  // Parse and calculate the score
   let totalScore = 0
-  const upperCaseWord = word.toUpperCase()
+  const currentMultiplier = 1
+  const globalMultiplier = 1
 
-  for (let i = 0; i < upperCaseWord.length; i++) {
-    const letter = upperCaseWord[i]
-    if (scores[letter]) {
-      totalScore += scores[letter]
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i]
+
+    if (char === '{' || char === '[') {
+      const multiplierType = char === '{' ? 2 : 3
+      const endChar = char === '{' ? '}' : ']'
+      const endIndex = word.indexOf(endChar, i + 1)
+
+      if (endIndex === -1) return 0
+
+      const subWord = word.substring(i + 1, endIndex)
+      const subScore = scrabble(subWord) * multiplierType
+      totalScore += subScore
+
+      i = endIndex
+    } else if (scores[char.toUpperCase()]) {
+      totalScore += scores[char.toUpperCase()] * currentMultiplier
     }
   }
 
-  return totalScore
+  return totalScore * globalMultiplier
 }
 
-console.log(scrabble(''))
-console.log(scrabble(' \t\n'))
-console.log(scrabble(null))
-console.log(scrabble('a'))
-console.log(scrabble('f'))
-console.log(scrabble('street'))
-console.log(scrabble('quirky'))
-console.log(scrabble('OXYPHENBUTAZONE'))
+console.log(scrabble('{dog}'))
+console.log(scrabble('[dog]'))
+console.log(scrabble('d{o}g'))
+console.log(scrabble('d[o]g'))
+console.log(scrabble('{[d]og}'))
+console.log(scrabble('[{dog}]'))
+console.log(scrabble('{d}o{g}'))
+console.log(scrabble('{dog'))
+console.log(scrabble('dog}'))
+console.log(scrabble('do}g'))
+console.log(scrabble('d{og'))
+console.log(scrabble('|d|og'))
 
 module.exports = scrabble
